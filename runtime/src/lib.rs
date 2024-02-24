da05db92588f3a0fccd24f8a058db65c7e1097ca
Peter White <petras9789@gmail.com>
2024-02-23 18:02:45 -0700

    chore: set runtime version to 0.1.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -179,7 +179,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pop-parachain"),
 	impl_name: create_runtime_str!("pop-parachain"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 0010,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
