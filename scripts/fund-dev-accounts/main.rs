6248e2a51fcb6e74d7e5f5bc3d2221a2794452ac
Peter White <petras9789@gmail.com>
2024-03-26 23:35:38 -0600

    feat: conditional compilation for rococo or paseo.

--- scripts/fund-dev-accounts/main.rs
+++ scripts/fund-dev-accounts/main.rs
@@ -1,91 +1,148 @@
+/// As Pop Network uses the relay chain token as native, the dev accounts are not funded by default.
+/// Therefore, after network launch there needs to be a reserve transfer from the relay chain
+/// to the dev accounts.
+///
+/// This script performs these reserve transfers to fund the dev accounts from the relay chain.
+///
 use subxt::{OnlineClient, PolkadotConfig};
 use subxt_signer::sr25519::{dev, Keypair};
 
 #[cfg(not(feature = "paseo"))]
 mod relay {
+	use super::*;
 	#[subxt::subxt(runtime_metadata_path = "../metadata/rococo.scale")]
 	pub(crate) mod runtime {}
 	pub(crate) type RuntimeCall = runtime::runtime_types::rococo_runtime::RuntimeCall;
 	pub(crate) const UNIT: u128 = 1_000_000_000_000;
+
+	pub(crate) use runtime::runtime_types::{
+		staging_xcm::v4::{
+			asset::Fungibility::Fungible,
+			asset::{Asset, AssetId, Assets},
+			junction::Junction,
+			junctions::Junctions,
+			junctions::Junctions::X1,
+			location::Location,
+		},
+		xcm::{v3::WeightLimit, VersionedAssets, VersionedLocation},
+	};
+
+	// generate XCM message to reserve transfer funds to a designated account on
+	// Pop Parachain
+	pub(crate) fn gen_account_fund_message_call(account: Keypair) -> RuntimeCall {
+		let pop_location = VersionedLocation::V4(Location {
+			parents: 0,
+			interior: X1([Junction::Parachain(9090)]),
+		});
+		let pop_beneficiary = VersionedLocation::V4(Location {
+			parents: 0,
+			interior: X1([Junction::AccountId32 { network: None, id: account.public_key().0 }]),
+		});
+		let amount = Fungible(UNIT * 1_000_000);
+		let assets = VersionedAssets::V4(Assets {
+			0: vec![Asset {
+				id: AssetId { 0: Location { parents: 0, interior: Junctions::Here } },
+				fun: amount,
+			}],
+		});
+
+		RuntimeCall::XcmPallet(
+			crate::relay::runtime::xcm_pallet::Call::limited_reserve_transfer_assets {
+				dest: Box::new(pop_location),
+				beneficiary: Box::new(pop_beneficiary),
+				assets: Box::new(assets),
+				fee_asset_item: 0,
+				weight_limit: WeightLimit::Unlimited,
+			},
+		)
+	}
 }
 
 #[cfg(feature = "paseo")]
 mod relay {
+	use super::*;
+
 	#[subxt::subxt(runtime_metadata_path = "../metadata/paseo.scale")]
 	pub(crate) mod runtime {}
-	pub(crate) type RuntimeCall = relay::runtime_types::paseo_runtime::RuntimeCall;
+	pub(crate) type RuntimeCall = runtime::runtime_types::paseo_runtime::RuntimeCall;
 	pub(crate) const UNIT: u128 = 10_000_000_000;
+
+	use runtime::runtime_types::{
+		staging_xcm::v3::multilocation::MultiLocation as Location,
+		xcm::{
+			v3::{
+				junction::Junction,
+				junctions::Junctions,
+				junctions::Junctions::X1,
+				multiasset::{
+					AssetId::Concrete, Fungibility::Fungible, MultiAsset as Asset,
+					MultiAssets as Assets,
+				},
+				WeightLimit,
+			},
+			VersionedMultiAssets as VersionedAssets, VersionedMultiLocation as VersionedLocation,
+		},
+	};
+
+	// generate XCM message to reserve transfer funds to a designated account on
+	// Pop Parachain
+	pub(crate) fn gen_account_fund_message_call(account: Keypair) -> RuntimeCall {
+		let pop_location =
+			VersionedLocation::V3(Location { parents: 0, interior: X1(Junction::Parachain(9090)) });
+		let pop_beneficiary = VersionedLocation::V3(Location {
+			parents: 0,
+			interior: X1(Junction::AccountId32 { network: None, id: account.public_key().0 }),
+		});
+		let amount = Fungible(UNIT * 1_000_000);
+
+		let assets = VersionedAssets::V3(Assets {
+			0: vec![Asset {
+				id: Concrete(Location { parents: 0, interior: Junctions::Here }),
+				fun: amount,
+			}],
+		});
+
+		RuntimeCall::XcmPallet(
+			crate::relay::runtime::xcm_pallet::Call::limited_reserve_transfer_assets {
+				dest: Box::new(pop_location),
+				beneficiary: Box::new(pop_beneficiary),
+				assets: Box::new(assets),
+				fee_asset_item: 0,
+				weight_limit: WeightLimit::Unlimited,
+			},
+		)
+	}
 }
 
-use relay::RuntimeCall;
-use relay::UNIT;
-
-use relay::runtime::runtime_types::{
-	staging_xcm::v4::{
-		asset::Fungibility::Fungible,
-		asset::{Asset, AssetId, Assets},
-		junction::Junction,
-		junctions::Junctions,
-		junctions::Junctions::X1,
-		location::Location,
-	},
-	xcm::{v3::WeightLimit, VersionedAssets, VersionedLocation},
-};
-
-// generate XCM message to reserve transfer funds to a designated account on
-// Pop Parachain
-fn gen_account_fund_message_call(account: Keypair) -> RuntimeCall {
-	let pop_location =
-		VersionedLocation::V4(Location { parents: 0, interior: X1([Junction::Parachain(9090)]) });
-	let pop_beneficiary = VersionedLocation::V4(Location {
-		parents: 0,
-		interior: X1([Junction::AccountId32 { network: None, id: account.public_key().0 }]),
-	});
-	let amount = Fungible(UNIT * 1_000_000);
-	let assets = VersionedAssets::V4(Assets {
-		0: vec![Asset {
-			id: AssetId { 0: Location { parents: 0, interior: Junctions::Here } },
-			fun: amount,
-		}],
-	});
-
-	RuntimeCall::XcmPallet(relay::runtime::xcm_pallet::Call::limited_reserve_transfer_assets {
-		dest: Box::new(pop_location),
-		beneficiary: Box::new(pop_beneficiary),
-		assets: Box::new(assets),
-		fee_asset_item: 0,
-		weight_limit: WeightLimit::Unlimited,
-	})
-}
+use relay::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let relay_api = OnlineClient::<PolkadotConfig>::from_url("ws://127.0.0.1:8833").await?;
+
 	let dev_accounts = vec![dev::alice(), dev::bob(), dev::charlie()];
 	let fund_pop_accounts =
 		dev_accounts.iter().map(|a| gen_account_fund_message_call(a.clone())).collect();
 
-	let rococo_api = OnlineClient::<PolkadotConfig>::from_url("ws://127.0.0.1:8833").await?;
-
-	let set_alice_balance =
-		RuntimeCall::Balances(relay::runtime::balances::Call::force_set_balance {
-			who: dev::alice().public_key().into(),
-			new_free: UNIT * 1_000_000_000,
-		});
+	let set_alice_balance = RuntimeCall::Balances(runtime::balances::Call::force_set_balance {
+		who: dev::alice().public_key().into(),
+		new_free: UNIT * 1_000_000_000,
+	});
 
 	// set alice's balance first so the account is guaranteed to have enough funds
-	let sudo_set_balance = relay::runtime::tx().sudo().sudo(set_alice_balance);
+	let sudo_set_balance = runtime::tx().sudo().sudo(set_alice_balance);
 	let from = dev::alice();
-	let _ = rococo_api
+	let _ = relay_api
 		.tx()
 		.sign_and_submit_then_watch_default(&sudo_set_balance, &from)
 		.await?
 		.wait_for_finalized_success()
 		.await?;
 
-	let batch_tx = relay::runtime::tx().utility().batch(fund_pop_accounts);
+	let batch_tx = runtime::tx().utility().batch(fund_pop_accounts);
 
 	let from = dev::alice();
-	let _ = rococo_api
+	let _ = relay_api
 		.tx()
 		.sign_and_submit_then_watch_default(&batch_tx, &from)
 		.await?
