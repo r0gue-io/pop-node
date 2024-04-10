150172a5d96b67e674514a467e51ffa1bf861297
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2024-04-10 17:59:25 +0200

    feat: pallet multisig (#69)
    * feat: pallet-multisig
    
    * feat: update pallet index

--- runtime/devnet/src/lib.rs
+++ runtime/devnet/src/lib.rs
@@ -559,6 +559,24 @@ impl pallet_preimage::Config for Runtime {
 	>;
 }
 
+parameter_types! {
+	// One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes.
+	pub const DepositBase: Balance = deposit(1, 88);
+	// Additional storage item size of 32 bytes.
+	pub const DepositFactor: Balance = deposit(0, 32);
+	pub const MaxSignatories: u32 = 100;
+}
+
+impl pallet_multisig::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeCall = RuntimeCall;
+	type Currency = Balances;
+	type DepositBase = DepositBase;
+	type DepositFactor = DepositFactor;
+	type MaxSignatories = MaxSignatories;
+	type WeightInfo = pallet_multisig::weights::SubstrateWeight<Runtime>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime {
@@ -596,6 +614,9 @@ construct_runtime!(
 
 		// Contracts
 		Contracts: pallet_contracts = 40,
+
+		// Multisig
+		Multisig: pallet_multisig = 42,
 
 		// Assets
 		Nfts: pallet_nfts = 50,
