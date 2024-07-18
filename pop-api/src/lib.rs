bfcd2bc598187ab746e7ad3fcfc215b2d94ec1f4
Daanvdplas <daanvdplas@live.nl>
2024-07-18 14:50:52 +0200

    docs: add docs to  code

--- pop-api/src/lib.rs
+++ pop-api/src/lib.rs
@@ -79,6 +79,19 @@ mod constants {
 	pub(crate) const BALANCES_MODULE: u8 = 10;
 }
 
+/// Represents a status code returned by the runtime.
+///
+/// `StatusCode` encapsulates a `u32` value that indicates the status of an operation performed
+/// by the runtime. It helps to communicate the success or failure of a chain extension method
+/// call to the contract, providing a standardized way to handle errors.
+///
+/// This status code can be used to determine if an operation succeeded or if it encountered
+/// an error. A `StatusCode` of `0` typically indicates success, while any other value represents
+/// an error.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct StatusCode(pub u32);
+
 impl From<u32> for StatusCode {
 	/// Converts a `u32` into a `StatusCode`.
 	fn from(value: u32) -> Self {
--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -156,24 +156,29 @@ pub mod v0 {
 	pub mod error {
 		use super::*;
 
+		/// Reason why a Pop API function call failed.
 		#[derive(Encode, Decode, Debug, Eq, PartialEq)]
 		#[cfg_attr(feature = "std", derive(TypeInfo))]
 		#[repr(u8)]
 		pub enum Error {
-			/// Some unknown error occurred. Go to the Pop API docs section `Pop API error`.
-			Other {
-				// Index within the `DispatchError`
-				dispatch_error_index: u8,
-				// Index within the `DispatchError` variant.
-				error_index: u8,
-				// Index for further nesting, e.g. pallet error.
-				error: u8,
-			},
+			/// An unknown error occurred. This variant captures any unexpected errors that the
+			/// contract cannot specifically handle. It is useful for cases where there are breaking
+			/// changes in the runtime or when an error falls outside the predefined categories. The
+			/// variant includes:
+			///
+			/// - `dispatch_error_index`: The index within the `DispatchError`.
+			/// - `error_index`: The index within the `DispatchError` variant (e.g. a `TokenError`).
+			/// - `error`: The specific error code or sub-index, providing additional context (e.g.
+			///   `error` in `ModuleError`).
+			Other { dispatch_error_index: u8, error_index: u8, error: u8 },
 			/// Failed to lookup some data.
 			CannotLookup,
 			/// A bad origin.
 			BadOrigin,
 			/// A custom error in a module.
+			///
+			/// - `index`: The pallet index.
+			/// - `error`: The error within the pallet.
 			Module { index: u8, error: u8 },
 			/// At least one consumer is remaining so the account cannot be destroyed.
 			ConsumerRemaining,
@@ -196,13 +201,18 @@ pub mod v0 {
 			Unavailable,
 			/// Root origin is not allowed.
 			RootNotAllowed,
-			/// Unknown function id.
-			UnknownFunctionId = 254,
-			/// Decoding failed on the runtime.
+			/// Unknown function called.
+			UnknownFunctionCall = 254,
+			/// Decoding failed.
 			DecodingFailed = 255,
 		}
 
 		impl From<u32> for Error {
+			/// Converts a `u32` status code into an `Error`.
+			///
+			/// This conversion maps a raw status code returned by the runtime into the more
+			/// descriptive `Error` enum variant, providing better context and understanding of the
+			/// error.
 			fn from(value: u32) -> Self {
 				let encoded = value.to_le_bytes();
 				Error::decode(&mut &encoded[..]).unwrap_or(Error::DecodingFailed)
@@ -210,6 +220,7 @@ pub mod v0 {
 		}
 
 		impl From<Error> for u32 {
+			/// Converts an `Error` to a `u32` status code.
 			fn from(value: Error) -> Self {
 				let mut encoded_error = value.encode();
 				// Resize the encoded value to 4 bytes in order to decode the value in a u32 (4 bytes).
@@ -220,6 +231,7 @@ pub mod v0 {
 			}
 		}
 
+		/// Description of what went wrong when trying to complete an operation on a token.
 		#[derive(Encode, Decode, Clone, Debug, MaxEncodedLen, Eq, PartialEq, Ord, PartialOrd)]
 		#[cfg_attr(feature = "std", derive(TypeInfo))]
 		pub enum TokenError {
@@ -246,6 +258,7 @@ pub mod v0 {
 			Blocked,
 		}
 
+		/// Arithmetic errors.
 		#[derive(Encode, Decode, Debug, Eq, PartialEq)]
 		#[cfg_attr(feature = "std", derive(TypeInfo))]
 		pub enum ArithmeticError {
@@ -257,6 +270,7 @@ pub mod v0 {
 			DivisionByZero,
 		}
 
+		/// Errors related to transactional storage layers.
 		#[derive(Encode, Decode, Debug, Eq, PartialEq)]
 		#[cfg_attr(feature = "std", derive(TypeInfo))]
 		pub enum TransactionalError {
