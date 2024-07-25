0e49f6309553ea596c7e576979166674483ed682
Daanvdplas <daanvdplas@live.nl>
2024-07-25 15:43:53 +0200

    fix: test devnet

--- runtime/devnet/src/extensions/v0.rs
+++ runtime/devnet/src/extensions/v0.rs
@@ -60,7 +60,7 @@ fn nested_errors(nested_error: &[u8], limit: Option<u8>) -> bool {
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use primitives::error::{
+	use pop_primitives::error::{
 		ArithmeticError::*,
 		Error::{self, *},
 		TokenError::*,
