29f1dad4e6e5a118d5f7ac1e6e56bfed2ea5f0af
Daan van der Plas <93204684+Daanvdplas@users.noreply.github.com>
2024-07-28 12:07:55 +0200

    refactor: remove read state encoding (#122)
    Co-authored-by: Frank Bell <frank@r0gue.io>

--- runtime/devnet/src/config/api.rs
+++ runtime/devnet/src/config/api.rs
@@ -11,24 +11,37 @@ pub enum RuntimeRead<T: fungibles::Config> {
 	Fungibles(fungibles::Read<T>),
 }
 
-impl fungibles::Config for Runtime {
-	type AssetsInstance = TrustBackedAssetsInstance;
-	type WeightInfo = fungibles::weights::SubstrateWeight<Runtime>;
-}
-
-/// A type to identify allowed calls to the Runtime from contracts. Used by Pop API
+/// A type to identify allowed calls to the Runtime from the API.
 pub struct AllowedApiCalls;
 
 impl Contains<RuntimeCall> for AllowedApiCalls {
+	/// Allowed runtime calls from the API.
 	fn contains(c: &RuntimeCall) -> bool {
-		use fungibles::Call as FungiblesCall;
+		use fungibles::Call::*;
 		matches!(
 			c,
-			RuntimeCall::Fungibles(
-				FungiblesCall::transfer { .. }
-					| FungiblesCall::approve { .. }
-					| FungiblesCall::increase_allowance { .. }
+			RuntimeCall::Fungibles(transfer { .. } | approve { .. } | increase_allowance { .. })
+		)
+	}
+}
+
+impl<T: fungibles::Config> Contains<RuntimeRead<T>> for AllowedApiCalls {
+	/// Allowed state queries from the API.
+	fn contains(c: &RuntimeRead<T>) -> bool {
+		use fungibles::Read::*;
+		matches!(
+			c,
+			RuntimeRead::Fungibles(
+				TotalSupply(..)
+					| BalanceOf { .. } | Allowance { .. }
+					| TokenName(..) | TokenSymbol(..)
+					| TokenDecimals(..)
 			)
 		)
 	}
+}
+
+impl fungibles::Config for Runtime {
+	type AssetsInstance = TrustBackedAssetsInstance;
+	type WeightInfo = fungibles::weights::SubstrateWeight<Runtime>;
 }
