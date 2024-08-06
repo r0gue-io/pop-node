c645105b43754512e294f6493e5bc93a28672fee
tin-snowflake <56880684+chungquantin@users.noreply.github.com>
2024-08-06 22:52:16 +0700

    refactor: trait implemented on enabled features

--- runtime/extensions/src/lib.rs
+++ runtime/extensions/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod constants;
-mod utils;
 mod v0;
 
 use codec::{Decode, Encode};
@@ -12,13 +11,14 @@ use frame_support::{
 use pallet_contracts::chain_extension::{
 	BufInBufOutState, ChainExtension, Environment, Ext, InitState, RetVal,
 };
-use pop_primitives::AssetId;
 use sp_core::crypto::UncheckedFrom;
 use sp_runtime::{traits::Dispatchable, DispatchError};
 use sp_std::vec::Vec;
-use utils::FuncId;
 
+#[cfg(feature = "pop-devnet")]
 pub(crate) use pallet_api::fungibles;
+#[cfg(feature = "pop-devnet")]
+use pop_primitives::AssetId;
 
 type ContractSchedule<T> = <T as pallet_contracts::Config>::Schedule;
 
@@ -44,12 +44,24 @@ pub trait DispatchCallParamsHandler {
 		T: PopApiExtensionConfig;
 }
 
+#[cfg(feature = "pop-devnet")]
 pub trait PopApiExtensionConfig:
 	frame_system::Config<RuntimeCall: GetDispatchInfo + Dispatchable<PostInfo = PostDispatchInfo>>
 	+ pallet_assets::Config<Self::AssetInstance, AssetId = AssetId>
 	+ fungibles::Config
 {
 	type AssetInstance;
+	type ReadStateParamsHandler: ReadStateParamsHandler;
+	type DispatchCallParamsHandler: DispatchCallParamsHandler;
+	type RuntimeRead: Encode + Decode + MaxEncodedLen;
+	type AllowedDispatchCalls: Contains<Self::RuntimeCall>;
+	type AllowedReadCalls: Contains<Self::RuntimeRead>;
+}
+
+#[cfg(not(feature = "pop-devnet"))]
+pub trait PopApiExtensionConfig:
+	frame_system::Config<RuntimeCall: GetDispatchInfo + Dispatchable<PostInfo = PostDispatchInfo>>
+{
 	type ReadStateParamsHandler: ReadStateParamsHandler;
 	type DispatchCallParamsHandler: DispatchCallParamsHandler;
 	type RuntimeRead: Encode + Decode + MaxEncodedLen;
@@ -240,4 +252,146 @@ pub(crate) fn convert_to_status_code(error: DispatchError, version: u8) -> u32 {
 		_ => encoded_error = UNKNOWN_CALL_ERROR_ENCODED,
 	}
 	u32::from_le_bytes(encoded_error)
+}
+
+/// Function identifiers used in the Pop API.
+///
+/// The `FuncId` specifies the available functions that can be called through the Pop API. Each
+/// variant corresponds to a specific functionality provided by the API, facilitating the
+/// interaction between smart contracts and the runtime.
+///
+/// - `Dispatch`: Represents a function call to dispatch a runtime call.
+/// - `ReadState`: Represents a function call to read the state from the runtime.
+/// - `SendXcm`: Represents a function call to send an XCM message.
+#[derive(Debug)]
+pub enum FuncId {
+	Dispatch,
+	ReadState,
+}
+
+impl TryFrom<u8> for FuncId {
+	type Error = DispatchError;
+
+	/// Attempts to convert a `u8` value to its corresponding `FuncId` variant.
+	///
+	/// If the `u8` value does not match any known function identifier, it returns a
+	/// `DispatchError::Other` indicating an unknown function ID.
+	fn try_from(func_id: u8) -> Result<Self, Self::Error> {
+		let id = match func_id {
+			0 => Self::Dispatch,
+			1 => Self::ReadState,
+			_ => {
+				return Err(UNKNOWN_CALL_ERROR);
+			},
+		};
+		Ok(id)
+	}
+}
+
+#[cfg(all(feature = "pop-devnet", test))]
+mod tests {
+	use super::*;
+
+	// Test ensuring `func_id()` and `ext_id()` work as expected, i.e. extracting the first two
+	// bytes and the last two bytes, respectively, from a 4 byte array.
+	#[test]
+	fn test_byte_extraction() {
+		use rand::Rng;
+
+		// Helper functions
+		fn func_id(id: u32) -> u16 {
+			(id & 0x0000FFFF) as u16
+		}
+		fn ext_id(id: u32) -> u16 {
+			(id >> 16) as u16
+		}
+
+		// Number of test iterations
+		let test_iterations = 1_000_000;
+
+		// Create a random number generator
+		let mut rng = rand::thread_rng();
+
+		// Run the test for a large number of random 4-byte arrays
+		for _ in 0..test_iterations {
+			// Generate a random 4-byte array
+			let bytes: [u8; 4] = rng.gen();
+
+			// Convert the 4-byte array to a u32 value
+			let value = u32::from_le_bytes(bytes);
+
+			// Extract the first two bytes (least significant 2 bytes)
+			let first_two_bytes = func_id(value);
+
+			// Extract the last two bytes (most significant 2 bytes)
+			let last_two_bytes = ext_id(value);
+
+			// Check if the first two bytes match the expected value
+			assert_eq!([bytes[0], bytes[1]], first_two_bytes.to_le_bytes());
+
+			// Check if the last two bytes match the expected value
+			assert_eq!([bytes[2], bytes[3]], last_two_bytes.to_le_bytes());
+		}
+	}
+
+	// Test showing all the different type of variants and its encoding.
+	#[test]
+	fn encoding_of_enum() {
+		#[derive(Debug, PartialEq, Encode, Decode)]
+		enum ComprehensiveEnum {
+			SimpleVariant,
+			DataVariant(u8),
+			NamedFields { w: u8 },
+			NestedEnum(InnerEnum),
+			OptionVariant(Option<u8>),
+			VecVariant(Vec<u8>),
+			TupleVariant(u8, u8),
+			NestedStructVariant(NestedStruct),
+			NestedEnumStructVariant(NestedEnumStruct),
+		}
+
+		#[derive(Debug, PartialEq, Encode, Decode)]
+		enum InnerEnum {
+			A,
+			B { inner_data: u8 },
+			C(u8),
+		}
+
+		#[derive(Debug, PartialEq, Encode, Decode)]
+		struct NestedStruct {
+			x: u8,
+			y: u8,
+		}
+
+		#[derive(Debug, PartialEq, Encode, Decode)]
+		struct NestedEnumStruct {
+			inner_enum: InnerEnum,
+		}
+
+		// Creating each possible variant for an enum.
+		let enum_simple = ComprehensiveEnum::SimpleVariant;
+		let enum_data = ComprehensiveEnum::DataVariant(42);
+		let enum_named = ComprehensiveEnum::NamedFields { w: 42 };
+		let enum_nested = ComprehensiveEnum::NestedEnum(InnerEnum::B { inner_data: 42 });
+		let enum_option = ComprehensiveEnum::OptionVariant(Some(42));
+		let enum_vec = ComprehensiveEnum::VecVariant(vec![1, 2, 3, 4, 5]);
+		let enum_tuple = ComprehensiveEnum::TupleVariant(42, 42);
+		let enum_nested_struct =
+			ComprehensiveEnum::NestedStructVariant(NestedStruct { x: 42, y: 42 });
+		let enum_nested_enum_struct =
+			ComprehensiveEnum::NestedEnumStructVariant(NestedEnumStruct {
+				inner_enum: InnerEnum::C(42),
+			});
+
+		// Encode and print each variant individually to see their encoded values.
+		println!("{:?} -> {:?}", enum_simple, enum_simple.encode());
+		println!("{:?} -> {:?}", enum_data, enum_data.encode());
+		println!("{:?} -> {:?}", enum_named, enum_named.encode());
+		println!("{:?} -> {:?}", enum_nested, enum_nested.encode());
+		println!("{:?} -> {:?}", enum_option, enum_option.encode());
+		println!("{:?} -> {:?}", enum_vec, enum_vec.encode());
+		println!("{:?} -> {:?}", enum_tuple, enum_tuple.encode());
+		println!("{:?} -> {:?}", enum_nested_struct, enum_nested_struct.encode());
+		println!("{:?} -> {:?}", enum_nested_enum_struct, enum_nested_enum_struct.encode());
+	}
 }
