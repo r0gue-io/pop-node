7943f0a8517d8d812e85951208c843192fb239ba
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-08-13 15:32:20 +0700

    refactor: remove native executor, add unified host functions

--- node/src/command.rs
+++ node/src/command.rs
@@ -16,7 +16,7 @@ use crate::{
 	chain_spec,
 	chain_spec::Relay,
 	cli::{Cli, RelayChainCli, Subcommand},
-	service::{new_partial, DevnetRuntimeExecutor, TestnetRuntimeExecutor},
+	service::new_partial,
 };
 
 #[derive(Debug, PartialEq)]
@@ -158,7 +158,7 @@ macro_rules! construct_async_run {
 		match runner.config().chain_spec.runtime() {
 			Runtime::Devnet => {
 				runner.async_run(|$config| {
-					let $components = new_partial::<pop_runtime_devnet::RuntimeApi, DevnetRuntimeExecutor>(
+					let $components = new_partial::<pop_runtime_devnet::RuntimeApi>(
 						&$config
 					)?;
 					let task_manager = $components.task_manager;
@@ -167,7 +167,7 @@ macro_rules! construct_async_run {
 			}
 			Runtime::Testnet => {
 				runner.async_run(|$config| {
-					let $components = new_partial::<pop_runtime_testnet::RuntimeApi, TestnetRuntimeExecutor>(
+					let $components = new_partial::<pop_runtime_testnet::RuntimeApi>(
 						&$config
 					)?;
 					let task_manager = $components.task_manager;
@@ -182,15 +182,11 @@ macro_rules! construct_benchmark_partials {
 	($config:expr, |$partials:ident| $code:expr) => {
 		match $config.chain_spec.runtime() {
 			Runtime::Devnet => {
-				let $partials =
-					new_partial::<pop_runtime_devnet::RuntimeApi, DevnetRuntimeExecutor>(&$config)?;
+				let $partials = new_partial::<pop_runtime_devnet::RuntimeApi>(&$config)?;
 				$code
 			},
 			Runtime::Testnet => {
-				let $partials = new_partial::<
-					pop_runtime_testnet::RuntimeApi,
-					TestnetRuntimeExecutor,
-				>(&$config)?;
+				let $partials = new_partial::<pop_runtime_testnet::RuntimeApi>(&$config)?;
 				$code
 			},
 		}
@@ -349,7 +345,6 @@ pub fn run() -> Result<()> {
 						);
 						crate::service::start_parachain_node::<
 							pop_runtime_devnet::RuntimeApi,
-							DevnetRuntimeExecutor,
 						>(
 							config,
 							polkadot_config,
@@ -367,7 +362,6 @@ pub fn run() -> Result<()> {
 						);
 						crate::service::start_parachain_node::<
 							pop_runtime_testnet::RuntimeApi,
-							TestnetRuntimeExecutor,
 						>(
 							config,
 							polkadot_config,
