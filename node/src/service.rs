d058b22d57d2a84249de14364444e3398b5a9196
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-08-13 15:32:20 +0700

    refactor: remove native executor, add unified host functions

--- node/src/service.rs
+++ node/src/service.rs
@@ -14,7 +14,8 @@ use cumulus_client_consensus_common::ParachainBlockImport as TParachainBlockImpo
 use cumulus_client_consensus_proposer::Proposer;
 use cumulus_client_service::{
 	build_network, build_relay_chain_interface, prepare_node_config, start_relay_chain_tasks,
-	BuildNetworkParams, CollatorSybilResistance, DARecoveryProfile, StartRelayChainTasksParams,
+	BuildNetworkParams, CollatorSybilResistance, DARecoveryProfile, ParachainHostFunctions,
+	StartRelayChainTasksParams,
 };
 use cumulus_primitives_core::{
 	relay_chain::{CollatorPair, ValidationCode},
