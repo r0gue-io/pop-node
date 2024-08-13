1e0fde7ebbee5dc9be45db3e70934bfe313b3e26
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-08-13 20:44:04 +0700

    chore: resolve conflicts

--- runtime/devnet/src/config/api.rs
+++ runtime/devnet/src/config/api.rs
@@ -1,12 +1,11 @@
-use crate::{config::assets::TrustBackedAssetsInstance, fungibles, Runtime, RuntimeCall};
 use crate::{
 	config::assets::TrustBackedAssetsInstance,
 	fungibles::{self},
-	Runtime,
+	Runtime, RuntimeCall,
 };
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::traits::Contains;
-use pop_chain_extension::ReadStateHandler;
+use pop_chain_extension::{CallFilter, ReadState};
+use sp_std::vec::Vec;
 
 /// A query of runtime state.
 #[derive(Encode, Decode, Debug, MaxEncodedLen)]
@@ -17,20 +16,36 @@ pub enum RuntimeRead {
 	Fungibles(fungibles::Read<Runtime>),
 }
 
-impl ReadStateHandler<Runtime> for RuntimeRead {
-	fn handle_read(read: RuntimeRead) -> sp_std::vec::Vec<u8> {
+/// A struct that implement requirements for the Pop API chain extension.
+#[derive(Default)]
+pub struct Extension;
+impl ReadState for Extension {
+	type StateQuery = RuntimeRead;
+
+	fn contains(c: &Self::StateQuery) -> bool {
+		use fungibles::Read::*;
+		matches!(
+			c,
+			RuntimeRead::Fungibles(
+				TotalSupply(..)
+					| BalanceOf { .. } | Allowance { .. }
+					| TokenName(..) | TokenSymbol(..)
+					| TokenDecimals(..) | AssetExists(..)
+			)
+		)
+	}
+
+	fn read(read: RuntimeRead) -> Vec<u8> {
 		match read {
 			RuntimeRead::Fungibles(key) => fungibles::Pallet::read_state(key),
 		}
 	}
 }
 
-/// A type to identify allowed calls to the Runtime from the API.
-pub struct AllowedApiCalls;
+impl CallFilter for Extension {
+	type Call = RuntimeCall;
 
-impl Contains<RuntimeCall> for AllowedApiCalls {
-	/// Allowed runtime calls from the API.
-	fn contains(c: &RuntimeCall) -> bool {
+	fn contains(c: &Self::Call) -> bool {
 		use fungibles::Call::*;
 		matches!(
 			c,
@@ -43,22 +58,6 @@ impl Contains<RuntimeCall> for AllowedApiCalls {
 					| start_destroy { .. }
 					| clear_metadata { .. }
 					| mint { .. } | burn { .. }
-			)
-		)
-	}
-}
-
-impl Contains<RuntimeRead> for AllowedApiCalls {
-	/// Allowed state queries from the API.
-	fn contains(c: &RuntimeRead) -> bool {
-		use fungibles::Read::*;
-		matches!(
-			c,
-			RuntimeRead::Fungibles(
-				TotalSupply(..)
-					| BalanceOf { .. } | Allowance { .. }
-					| TokenName(..) | TokenSymbol(..)
-					| TokenDecimals(..) | AssetExists(..)
 			)
 		)
 	}
