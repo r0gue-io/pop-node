b8d61ea89a20d75607c78f5d342d3e6384cd9ad8
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-08-14 17:11:28 +0700

    chore: rebase v1.10.0

--- runtime/testnet/build.rs
+++ runtime/testnet/build.rs
@@ -1,4 +1,14 @@
-#[cfg(feature = "std")]
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::new()
+		.with_current_project()
+		.export_heap_base()
+		.import_memory()
+		.enable_metadata_hash("UNIT", 12)
+		.build()
+}
+
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::new()
 		.with_current_project()
