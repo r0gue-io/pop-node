8eeadeca4de1a42c15e8a97d9290d91109e412fb
Tin Chung <56880684+chungquantin@users.noreply.github.com>
2024-08-16 16:37:40 +0700

    chore: upgrade to 1.12.0 (#193)

--- runtime/devnet/build.rs
+++ runtime/devnet/build.rs
@@ -1,11 +1,4 @@
-#[cfg(all(feature = "std", feature = "metadata-hash"))]
-fn main() {
-	substrate_wasm_builder::WasmBuilder::init_with_defaults()
-		.enable_metadata_hash("UNIT", 12)
-		.build()
-}
-
-#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
+#[cfg(feature = "std")]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::build_using_defaults()
 }
