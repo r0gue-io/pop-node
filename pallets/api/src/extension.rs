65387226fca3933f2ed918fb5b2044070f23641a
Frank Bell <frank@r0gue.io>
2024-08-20 11:13:43 +0100

    refactor: improve logging

--- pallets/api/src/extension.rs
+++ pallets/api/src/extension.rs
@@ -1,16 +1,21 @@
 use core::marker::PhantomData;
 use frame_support::traits::Get;
 pub use pop_chain_extension::{Config, DispatchCall, ReadState, Readable};
-use pop_chain_extension::{Decodes, Environment, Ext, Matches, Processor, State};
+use pop_chain_extension::{Decodes, Environment, Ext, LogTarget, Matches, Processor, State};
 
+/// The logging target for the chain extension.
+pub const LOG_TARGET: &str = "pop-api::extension";
+
+/// The chain extension used by the API.
 pub type Extension<Functions> = pop_chain_extension::Extension<Functions>;
-
 /// Decodes output by prepending bytes from ext_id() + func_id()
-pub type DecodesAs<Output> = Decodes<Output, Prepender>;
+pub type DecodesAs<Output, Logger = ()> = Decodes<Output, Prepender, Logger>;
 
 /// Prepends bytes from ext_id() + func_id() to prefix the encoded input bytes to determine the versioned output
 pub struct Prepender;
 impl Processor for Prepender {
+	const LOG_TARGET: &'static str = "pop-api::extension::processor";
+
 	fn process<E: Ext, S: State>(value: &mut Vec<u8>, env: &mut Environment<E, S>) {
 		// TODO: revisit the ordering based on specced standard
 		// Resolve version, pallet and call index from environment
@@ -23,6 +28,7 @@ impl Processor for Prepender {
 		value.insert(0, version);
 		value.insert(1, pallet_index);
 		value.insert(2, call_index);
+		log::debug!(target: Self::LOG_TARGET, "prepender: version={version}, module={pallet_index}, call={call_index}")
 	}
 }
 
@@ -33,4 +39,16 @@ impl<T: Get<u8>> Matches for IdentifiedByFirstByteOfFunctionId<T> {
 		let bytes = func_id.to_le_bytes();
 		bytes[1] == T::get()
 	}
+}
+
+/// A log target for dispatched calls.
+pub struct DispatchCallLogTarget;
+impl LogTarget for DispatchCallLogTarget {
+	const LOG_TARGET: &'static str = "pop-api::extension::dispatch";
+}
+
+/// A log target for state reads.
+pub struct ReadStateLogTarget;
+impl LogTarget for ReadStateLogTarget {
+	const LOG_TARGET: &'static str = "pop-api::extension::read-state";
 }
