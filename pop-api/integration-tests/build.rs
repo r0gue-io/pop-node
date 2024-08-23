f295707fe1bf3f575d9699a5106653973c87de1e
Tin Chung <56880684+chungquantin@users.noreply.github.com>
2024-08-24 00:24:47 +0700

    refactor: build script
    Co-authored-by: Daan van der Plas <93204684+Daanvdplas@users.noreply.github.com>

--- pop-api/integration-tests/build.rs
+++ pop-api/integration-tests/build.rs
@@ -1,36 +1,52 @@
-use contract_build::{execute, BuildMode, ExecuteArgs, ManifestPath, Verbosity};
-use std::fs;
-use std::path::{Path, PathBuf};
+use contract_build::{execute, BuildMode, BuildResult, ExecuteArgs, ManifestPath, Verbosity};
+use std::{
+	fs,
+	path::{Path, PathBuf},
+	process,
+};
 
 fn main() {
-	let contracts = PathBuf::from("./contracts/");
-	for contract in &get_subcontract_directories(&contracts) {
-		let manifest_path = ManifestPath::new(contract.join("Cargo.toml"))
-			.expect(&format!("Failed to retrieve contract: {:?}", contract));
-		let args = ExecuteArgs {
-			build_mode: BuildMode::Release,
-			manifest_path,
-			verbosity: Verbosity::Quiet,
-			..Default::default()
-		};
-		execute(args).expect(&format!("Failed to build contract: {:?}", contract));
+	let contracts_dir = PathBuf::from("./contracts/");
+	let contract_dirs = match get_subcontract_directories(&contracts_dir) {
+		Ok(dirs) => dirs,
+		Err(e) => {
+			eprintln!("Failed to read contracts directory: {}", e);
+			process::exit(1);
+		},
+	};
+
+	for contract in contract_dirs {
+		if let Err(e) = build_contract(&contract) {
+			eprintln!("Failed to build contract {}: {}", contract.display(), e);
+			process::exit(1);
+		}
 	}
 }
 
 // Function to retrieve all subdirectories in a given directory.
-fn get_subcontract_directories(contracts_dir: &Path) -> Vec<PathBuf> {
-	let mut directories = Vec::new();
-	if let Ok(entries) = fs::read_dir(contracts_dir) {
-		for entry in entries {
-			if let Ok(entry) = entry {
-				let path = entry.path();
-				if path.is_dir() {
-					directories.push(path);
-				}
-			}
-		}
-	} else {
-		panic!("Failed to read contracts directory: {:?}", contracts_dir);
-	}
-	directories
+fn get_subcontract_directories(contracts_dir: &Path) -> Result<Vec<PathBuf>, String> {
+	fs::read_dir(contracts_dir)
+		.map_err(|e| format!("Could not read directory '{}': {}", contracts_dir.display(), e))?
+		.filter_map(|entry| match entry {
+			Ok(entry) if entry.path().is_dir() => Some(Ok(entry.path())),
+			Ok(_) => None,
+			Err(e) => Some(Err(format!("Error reading directory entry: {}", e))),
+		})
+		.collect()
+}
+
+// Function to build a contract given its directory.
+fn build_contract(contract_dir: &Path) -> Result<BuildResult, String> {
+	let manifest_path = ManifestPath::new(contract_dir.join("Cargo.toml")).map_err(|e| {
+		format!("Could not retrieve manifest path for {}: {}", contract_dir.display(), e)
+	})?;
+
+	let args = ExecuteArgs {
+		build_mode: BuildMode::Release,
+		manifest_path,
+		verbosity: Verbosity::Quiet,
+		..Default::default()
+	};
+
+	execute(args).map_err(|e| format!("Build failed for {}: {}", contract_dir.display(), e))
 }
