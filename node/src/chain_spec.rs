022a33a724edd77bb1f70e9916506faf4d0184fe
Peter White <petras9789@gmail.com>
2024-08-28 16:50:11 -0600

    set sudo address in chain spec

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -206,8 +206,15 @@ pub fn mainnet_config(relay: Relay) -> MainnetChainSpec {
 		AccountId::from_ss58check("5GMqrQuWpyyBBK7LAWXR5psWvKc1QMqtiyasjp23VNKZWgh6").unwrap();
 	let collator_2_aura_id: AuraId =
 		AuraId::from_ss58check("5GMqrQuWpyyBBK7LAWXR5psWvKc1QMqtiyasjp23VNKZWgh6").unwrap();
+
+	// Multisig account for sudo, generated from the following signatories:
+	// - 15VPagCVayS6XvT5RogPYop3BJTJzwqR2mCGR1kVn3w58ygg
+	// - 142zako1kfvrpQ7pJKYR8iGUD58i4wjb78FUsmJ9WcXmkM5z
+	// - 15k9niqckMg338cFBoz9vWFGwnCtwPBquKvqJEfHApijZkDz
+	// - 14G3CUFnZUBnHZUhahexSZ6AgemaW9zMHBnGccy3df7actf4
+	// - Threshold 2
 	let sudo_account_id: AccountId =
-		AccountId::from_ss58check("5FPL3ZLqUk6MyBoZrQZ1Co29WAteX6T6N68TZ6jitHvhpyuD").unwrap();
+		AccountId::from_ss58check("15NMV2JX1NeMwarQiiZvuJ8ixUcvayFDcu1F9Wz1HNpSc8gP").unwrap();
 
 	#[allow(deprecated)]
 	MainnetChainSpec::builder(
