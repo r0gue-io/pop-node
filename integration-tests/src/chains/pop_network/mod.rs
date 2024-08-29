032fca4692b224af2e915c1aeade2112c4edfb92
Peter White <petras9789@gmail.com>
2024-08-29 14:03:20 -0600

    refactor(integration-tests): remove unused assets pallet  in XCM tests

--- integration-tests/src/chains/pop_network/mod.rs
+++ integration-tests/src/chains/pop_network/mod.rs
@@ -2,8 +2,7 @@ pub(crate) mod genesis;
 
 use emulated_integration_tests_common::{
 	impl_accounts_helpers_for_parachain, impl_assert_events_helpers_for_parachain,
-	impl_assets_helpers_for_parachain, impl_xcm_helpers_for_parachain, impls::Parachain,
-	xcm_emulator::decl_test_parachains,
+	impl_xcm_helpers_for_parachain, impls::Parachain, xcm_emulator::decl_test_parachains,
 };
 use frame_support::traits::OnInitialize;
 #[cfg(not(feature = "mainnet"))]
@@ -12,29 +11,6 @@ use pop_runtime_devnet as runtime;
 use pop_runtime_mainnet as runtime;
 
 // PopNetwork Parachain declaration
-#[cfg(not(feature = "mainnet"))]
-decl_test_parachains! {
-	pub struct PopNetwork {
-		genesis = genesis::genesis(),
-		on_init = {
-			runtime::AuraExt::on_initialize(1);
-		},
-		runtime = runtime,
-		core = {
-			XcmpMessageHandler: runtime::XcmpQueue,
-			LocationToAccountId: runtime::config::xcm::LocationToAccountId,
-			ParachainInfo: runtime::ParachainInfo,
-			MessageOrigin: cumulus_primitives_core::AggregateMessageOrigin,
-		},
-		pallets = {
-			PolkadotXcm: runtime::PolkadotXcm,
-			Balances: runtime::Balances,
-			Assets: runtime::Assets,
-		}
-	},
-}
-
-#[cfg(feature = "mainnet")]
 decl_test_parachains! {
 	pub struct PopNetwork {
 		genesis = genesis::genesis(),
@@ -59,5 +35,3 @@ decl_test_parachains! {
 impl_accounts_helpers_for_parachain!(PopNetwork);
 impl_assert_events_helpers_for_parachain!(PopNetwork);
 impl_xcm_helpers_for_parachain!(PopNetwork);
-#[cfg(not(feature = "mainnet"))]
-impl_assets_helpers_for_parachain!(PopNetwork);
