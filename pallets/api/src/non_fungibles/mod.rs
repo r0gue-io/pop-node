dd80c105ec3fe2628b3b29ffcce3b31cfccbd7d5
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-09-04 16:03:15 +0700

    feat: add pallet function mint, burn, transfer

--- pallets/api/src/non_fungibles/mod.rs
+++ pallets/api/src/non_fungibles/mod.rs
@@ -4,7 +4,21 @@
 
 pub use pallet::*;
 
+use frame_support::traits::nonfungibles_v2::Inspect;
+use pallet_nfts::WeightInfo;
+use sp_runtime::traits::StaticLookup;
+
 type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+type NonFungiblesInstanceOf<T> = <T as Config>::NonFungiblesInstance;
+type NonFungiblesOf<T> = pallet_nfts::Pallet<T, NonFungiblesInstanceOf<T>>;
+type NonFungiblesWeightInfoOf<T> =
+	<T as pallet_nfts::Config<NonFungiblesInstanceOf<T>>>::WeightInfo;
+type CollectionIdOf<T> = <pallet_nfts::Pallet<T, NonFungiblesInstanceOf<T>> as Inspect<
+	<T as frame_system::Config>::AccountId,
+>>::CollectionId;
+type ItemIdOf<T> = <pallet_nfts::Pallet<T, NonFungiblesInstanceOf<T>> as Inspect<
+	<T as frame_system::Config>::AccountId,
+>>::ItemId;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -24,9 +38,13 @@ pub mod pallet {
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config:
+		frame_system::Config + pallet_nfts::Config<Self::NonFungiblesInstance>
+	{
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		/// The instance of pallet nfts it is tightly coupled to.
+		type NonFungiblesInstance;
 	}
 
 	#[pallet::pallet]
@@ -39,9 +57,59 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// Create a new non-fungible token to the collection.
 		#[pallet::call_index(0)]
+		#[pallet::weight(NonFungiblesWeightInfoOf::<T>::mint())]
+		pub fn mint_to(
+			origin: OriginFor<T>,
+			to: AccountIdOf<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+		) -> DispatchResult {
+			NonFungiblesOf::<T>::mint(
+				origin,
+				collection,
+				item,
+				T::Lookup::unlookup(to.clone()),
+				None,
+			)?;
+			Ok(())
+		}
+
+		/// Destroy a new non-fungible token to the collection.
+		#[pallet::call_index(1)]
+		#[pallet::weight(NonFungiblesWeightInfoOf::<T>::burn())]
+		pub fn burn(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+		) -> DispatchResult {
+			NonFungiblesOf::<T>::burn(origin, collection, item)?;
+			Ok(())
+		}
+
+		/// Transfer a token from one account to the another account.
+		#[pallet::call_index(2)]
+		#[pallet::weight(NonFungiblesWeightInfoOf::<T>::transfer())]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+			dest: AccountIdOf<T>,
+		) -> DispatchResult {
+			NonFungiblesOf::<T>::transfer(
+				origin,
+				collection,
+				item,
+				T::Lookup::unlookup(dest.clone()),
+			)?;
+			Ok(())
+		}
+
+		/// Delegate a permission to perform actions on the collection item to an account.
+		#[pallet::call_index(3)]
 		#[pallet::weight(Weight::default())]
-		pub fn transfer(_origin: OriginFor<T>) -> DispatchResult {
+		pub fn approve(_origin: OriginFor<T>) -> DispatchResult {
 			Ok(())
 		}
 	}
