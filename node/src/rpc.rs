acd0839fbb03ba20dcf6e30f7620aee9ca5448be
Peter White <petras9789@gmail.com>
2024-09-09 19:14:20 -0600

    refactor(node): feature gate ISMP in node. Disables testnet and mainnet with --ismp enabled

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -7,7 +7,7 @@
 
 use std::sync::Arc;
 
-use pallet_ismp_rpc::{IsmpApiServer, IsmpRpcHandler};
+use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
 use pop_runtime_common::{AccountId, Balance, Block, Nonce};
 use sc_client_api::{AuxStore, BlockBackend, ProofProvider};
 pub use sc_rpc::DenyUnsafe;
@@ -15,7 +15,9 @@ use sc_transaction_pool_api::TransactionPool;
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
+#[cfg(feature = "ismp")]
 use sp_core::H256;
+use substrate_frame_rpc_system::{System, SystemApiServer};
 
 /// A type representing all RPC extensions.
 pub type RpcExtension = jsonrpsee::RpcModule<()>;
@@ -33,6 +35,38 @@ pub struct FullDeps<C, P, B> {
 }
 
 /// Instantiate all RPC extensions.
+#[cfg(not(feature = "ismp"))]
+pub fn create_full<C, P, B>(
+	deps: FullDeps<C, P, B>,
+) -> Result<RpcExtension, Box<dyn std::error::Error + Send + Sync>>
+where
+	C: ProvideRuntimeApi<Block>
+		+ HeaderBackend<Block>
+		+ AuxStore
+		+ BlockBackend<Block>
+		+ ProofProvider<Block>
+		+ HeaderMetadata<Block, Error = BlockChainError>
+		+ Send
+		+ Sync
+		+ 'static,
+	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
+	C::Api: BlockBuilder<Block>,
+	P: TransactionPool + Sync + Send + 'static,
+	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
+	B::State: sc_client_api::StateBackend<sp_runtime::traits::HashingFor<Block>>,
+{
+	let mut module = RpcExtension::new(());
+	let FullDeps { client, pool, deny_unsafe, backend: _ } = deps;
+
+	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
+	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+
+	Ok(module)
+}
+
+/// Instantiate all RPC extensions.
+#[cfg(feature = "ismp")]
 pub fn create_full<C, P, B>(
 	deps: FullDeps<C, P, B>,
 ) -> Result<RpcExtension, Box<dyn std::error::Error + Send + Sync>>
@@ -54,14 +88,14 @@ where
 	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
 	B::State: sc_client_api::StateBackend<sp_runtime::traits::HashingFor<Block>>,
 {
-	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
-	use substrate_frame_rpc_system::{System, SystemApiServer};
-
 	let mut module = RpcExtension::new(());
 	let FullDeps { client, pool, deny_unsafe, backend } = deps;
 
 	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
 	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
+
+	use pallet_ismp_rpc::{IsmpApiServer, IsmpRpcHandler};
 	module.merge(IsmpRpcHandler::new(client, backend.clone())?.into_rpc())?;
+
 	Ok(module)
 }
