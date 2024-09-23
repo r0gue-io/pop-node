cbcf66a1425ec0e8cac6f1f2f954b27d7fd3bb87
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-09-23 15:29:40 +0700

    chore: revert example contract changes

--- pop-api/examples/fungibles/lib.rs
+++ pop-api/examples/fungibles/lib.rs
@@ -2,15 +2,12 @@
 
 use ink::prelude::vec::Vec;
 use pop_api::{
+	fungibles::{self as api},
 	primitives::TokenId,
-	v0::fungibles::{self as api},
 	StatusCode,
 };
 
-#[cfg(test)]
-mod tests;
-
-type PopApiResult<T> = core::result::Result<T, StatusCode>;
+pub type Result<T> = core::result::Result<T, StatusCode>;
 
 #[ink::contract]
 mod fungibles {
@@ -27,27 +24,27 @@ mod fungibles {
 		}
 
 		#[ink(message)]
-		pub fn mint(
-			&mut self,
-			token: TokenId,
-			account: AccountId,
-			amount: Balance,
-		) -> PopApiResult<()> {
-			api::mint(token, account, amount)
+		pub fn total_supply(&self, token: TokenId) -> Result<Balance> {
+			api::total_supply(token)
 		}
 
 		#[ink(message)]
-		pub fn burn(
-			&mut self,
-			token: TokenId,
-			account: AccountId,
-			amount: Balance,
-		) -> PopApiResult<()> {
-			api::burn(token, account, amount)
+		pub fn balance_of(&self, token: TokenId, owner: AccountId) -> Result<Balance> {
+			api::balance_of(token, owner)
 		}
 
 		#[ink(message)]
-		pub fn transfer(&mut self, token: TokenId, to: AccountId, value: Balance) -> PopApiResult<()> {
+		pub fn allowance(
+			&self,
+			token: TokenId,
+			owner: AccountId,
+			spender: AccountId,
+		) -> Result<Balance> {
+			api::allowance(token, owner, spender)
+		}
+
+		#[ink(message)]
+		pub fn transfer(&mut self, token: TokenId, to: AccountId, value: Balance) -> Result<()> {
 			api::transfer(token, to, value)
 		}
 
@@ -59,7 +56,7 @@ mod fungibles {
 			to: AccountId,
 			value: Balance,
 			_data: Vec<u8>,
-		) -> PopApiResult<()> {
+		) -> Result<()> {
 			api::transfer_from(token, from, to, value)
 		}
 
@@ -69,7 +66,7 @@ mod fungibles {
 			token: TokenId,
 			spender: AccountId,
 			value: Balance,
-		) -> PopApiResult<()> {
+		) -> Result<()> {
 			api::approve(token, spender, value)
 		}
 
@@ -79,7 +76,7 @@ mod fungibles {
 			token: TokenId,
 			spender: AccountId,
 			value: Balance,
-		) -> PopApiResult<()> {
+		) -> Result<()> {
 			api::increase_allowance(token, spender, value)
 		}
 
@@ -89,71 +86,23 @@ mod fungibles {
 			token: TokenId,
 			spender: AccountId,
 			value: Balance,
-		) -> PopApiResult<()> {
+		) -> Result<()> {
 			api::decrease_allowance(token, spender, value)
 		}
 
-		#[ink(message, payable)]
-		pub fn create(
-			&self,
-			id: TokenId,
-			admin: AccountId,
-			min_balance: Balance,
-		) -> PopApiResult<()> {
-			api::create(id, admin, min_balance)?;
-			self.env().emit_event(api::events::Created { id, creator: admin, admin });
-			Ok(())
-		}
-
 		#[ink(message)]
-		pub fn set_metadata(
-			&self,
-			token: TokenId,
-			name: Vec<u8>,
-			symbol: Vec<u8>,
-			decimals: u8,
-		) -> PopApiResult<()> {
-			api::set_metadata(token, name, symbol, decimals)
-		}
-
-		#[ink(message)]
-		pub fn total_supply(&self, token: TokenId) -> PopApiResult<Balance> {
-			api::total_supply(token)
-		}
-
-		#[ink(message)]
-		pub fn balance_of(&self, token: TokenId, owner: AccountId) -> PopApiResult<Balance> {
-			api::balance_of(token, owner)
-		}
-
-		#[ink(message)]
-		pub fn allowance(
-			&self,
-			token: TokenId,
-			owner: AccountId,
-			spender: AccountId,
-		) -> PopApiResult<Balance> {
-			api::allowance(token, owner, spender)
-		}
-
-		#[ink(message)]
-		pub fn token_name(&self, token: TokenId) -> PopApiResult<Vec<u8>> {
+		pub fn token_name(&self, token: TokenId) -> Result<Vec<u8>> {
 			api::token_name(token)
 		}
 
 		#[ink(message)]
-		pub fn token_symbol(&self, token: TokenId) -> PopApiResult<Vec<u8>> {
+		pub fn token_symbol(&self, token: TokenId) -> Result<Vec<u8>> {
 			api::token_symbol(token)
 		}
 
 		#[ink(message)]
-		pub fn token_decimals(&self, token: TokenId) -> PopApiResult<u8> {
+		pub fn token_decimals(&self, token: TokenId) -> Result<u8> {
 			api::token_decimals(token)
-		}
-
-		#[ink(message)]
-		pub fn token_exists(&self, token: TokenId) -> PopApiResult<bool> {
-			api::token_exists(token)
 		}
 	}
 }
