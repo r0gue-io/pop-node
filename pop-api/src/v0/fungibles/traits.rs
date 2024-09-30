5937c5d016cadf3568e2f5092be09b7b13f1f182
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-09-30 21:46:52 +0700

    feat: add function selectors to psp22 trait

--- pop-api/src/v0/fungibles/traits.rs
+++ pop-api/src/v0/fungibles/traits.rs
@@ -8,43 +8,68 @@ use ink::prelude::string::String;
 #[ink::trait_definition]
 pub trait Psp22 {
 	/// Returns the total token supply.
-	#[ink(message)]
+	///
+	/// The selector is `0x162df8c2` (first 4 bytes of `blake2b_256("PSP22::total_supply")`).
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#total_supply--balance
+	#[ink(function = 0x162df8c2)]
 	fn total_supply(&self) -> Balance;
 
 	/// Returns the account balance for the specified `owner`.
 	///
+	/// The selector is `0x6568382f` (first 4 bytes of `blake2b_256("PSP22::balance_of")`).
+	///
 	/// # Parameters
 	/// - `owner` - The account whose balance is being queried.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#balance_ofowner-accountid--balance
+	#[ink(function = 0x6568382f)]
 	fn balance_of(&self, owner: AccountId) -> Balance;
 
 	/// Returns the allowance for a `spender` approved by an `owner`.
 	///
+	/// The selector is `0x4d47d921` (first 4 bytes of `blake2b_256("PSP22::allowance")`).
+	///
 	/// # Parameters
 	/// - `owner` - The account that owns the tokens.
 	/// - `spender` - The account that is allowed to spend the tokens.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#allowanceowner-accountid-spender-accountid--balance
+	#[ink(function = 0x4d47d921)]
 	fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance;
 
 	/// Transfers `value` amount of tokens from the caller's account to account `to`
 	/// with additional `data` in unspecified format.
 	///
+	/// The selector is `0xdb20f9f5` (first 4 bytes of `blake2b_256("PSP22::transfer")`).
+	///
 	/// # Parameters
 	/// - `to` - The recipient account.
 	/// - `value` - The number of tokens to transfer.
 	/// - `data` - Additional data in unspecified format.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#transferto-accountid-value-balance-data-u8--result-psp22error
+	#[ink(function = 0xdb20f9f5)]
 	fn transfer(&mut self, to: AccountId, value: Balance, data: Vec<u8>) -> Result<(), PSP22Error>;
 
 	/// Transfers `value` tokens on behalf of `from` to the account `to`
 	/// with additional `data` in unspecified format.
+	///
+	/// The selector is `0x54b3c76e` (first 4 bytes of `blake2b_256("PSP22::transfer_from")`).
 	///
 	/// # Parameters
 	/// - `from` - The account from which the token balance will be withdrawn.
 	/// - `to` - The recipient account.
 	/// - `value` - The number of tokens to transfer.
 	/// - `data` - Additional data with unspecified format.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#transfer_fromfrom-accountid-to-accountid-value-balance-data-u8--result-psp22error
+	#[ink(function = 0x54b3c76e)]
 	fn transfer_from(
 		&mut self,
 		from: AccountId,
@@ -55,28 +80,43 @@ pub trait Psp22 {
 
 	/// Approves `spender` to spend `value` amount of tokens on behalf of the caller.
 	///
+	/// The selector is `0xb20f1bbd` (first 4 bytes of `blake2b_256("PSP22::approve")`).
+	///
 	/// Successive calls of this method overwrite previous values.
 	///
 	/// # Parameters
 	/// - `spender` - The account that is allowed to spend the tokens.
 	/// - `value` - The number of tokens to approve.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#approvespender-accountid-value-balance--result-psp22error
+	#[ink(function = 0xb20f1bbd)]
 	fn approve(&mut self, spender: AccountId, value: Balance) -> Result<(), PSP22Error>;
 
 	/// Increases the allowance of `spender` by `value` amount of tokens.
 	///
+	/// The selector is `0x96d6b57a` (first 4 bytes of `blake2b_256("PSP22::increase_allowance")`).
+	///
 	/// # Parameters
 	/// - `spender` - The account that is allowed to spend the tokens.
 	/// - `value` - The number of tokens to increase the allowance by.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#increase_allowancespender-accountid-delta_value-balance--result-psp22error
+	#[ink(function = 0x96d6b57a)]
 	fn increase_allowance(&mut self, spender: AccountId, value: Balance) -> Result<(), PSP22Error>;
 
 	/// Decreases the allowance of `spender` by `value` amount of tokens.
 	///
+	/// The selector is `0xfecb57d5` (first 4 bytes of `blake2b_256("PSP22::decrease_allowance")`).
+	///
 	/// # Parameters
 	/// - `spender` - The account that is allowed to spend the tokens.
 	/// - `value` - The number of tokens to decrease the allowance by.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#decrease_allowancespender-accountid-delta_value-balance--result-psp22error
+	#[ink(function = 0xfecb57d5)]
 	fn decrease_allowance(&mut self, spender: AccountId, value: Balance) -> Result<(), PSP22Error>;
 }
 
@@ -84,15 +124,30 @@ pub trait Psp22 {
 #[ink::trait_definition]
 pub trait Psp22Metadata {
 	/// Returns the token name.
-	#[ink(message)]
+	///
+	/// The selector is `0x3d261bd4` (first 4 bytes of `blake2b_256("PSP22Metadata::token_name")`).
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#token_name--option
+	#[ink(function = 0x3d261bd4)]
 	fn token_name(&self) -> Option<String>;
 
 	/// Returns the token symbol.
-	#[ink(message)]
+	///
+	/// The selector is `0x34205be5` (first 4 bytes of `blake2b_256("PSP22Metadata::token_symbol")`).
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#token_symbol--option
+	#[ink(function = 0x34205be5)]
 	fn token_symbol(&self) -> Option<String>;
 
 	/// Returns the token decimals.
-	#[ink(message)]
+	///
+	/// The selector is `0x7271b782` (first 4 bytes of `blake2b_256("PSP22Metadata::token_decimals")`).
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#token_decimals--u8
+	#[ink(function = 0x7271b782)]
 	fn token_decimals(&self) -> u8;
 }
 
@@ -101,10 +156,15 @@ pub trait Psp22Metadata {
 pub trait Psp22Mintable {
 	/// Creates `value` amount of tokens and assigns them to `account`, increasing the total supply.
 	///
+	/// The selector is `0xfc3c75d4` (first 4 bytes of `blake2b_256("PSP22Mintable::mint")`).
+	///
 	/// # Parameters
 	/// - `account` - The account to be credited with the created tokens.
 	/// - `value` - The number of tokens to mint.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#token_decimals--u8
+	#[ink(function = 0xfc3c75d4)]
 	fn mint(&mut self, account: AccountId, value: Balance) -> Result<(), PSP22Error>;
 }
 
@@ -113,9 +173,14 @@ pub trait Psp22Mintable {
 pub trait Psp22Burnable {
 	/// Destroys `value` amount of tokens from `account`, reducing the total supply.
 	///
+	/// The selector is `0x7a9da510` (first 4 bytes of `blake2b_256("PSP22Burnable::burn")`).
+	///
 	/// # Parameters
 	/// - `account` - The account from which the tokens will be destroyed.
 	/// - `value` - The number of tokens to destroy.
-	#[ink(message)]
+	///
+	/// # Reference
+	/// https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md#token_decimals--u8
+	#[ink(function = 0x7a9da510)]
 	fn burn(&mut self, account: AccountId, value: Balance) -> Result<(), PSP22Error>;
 }
