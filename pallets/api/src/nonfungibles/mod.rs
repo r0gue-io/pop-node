9c5eb78718059e14c5bb167597f7d31f4bf293db
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-17 19:45:16 +0700

    feat(nonfungibles + nfts): add new methods to manage attributes

--- pallets/api/src/nonfungibles/mod.rs
+++ pallets/api/src/nonfungibles/mod.rs
@@ -14,11 +14,14 @@ mod types;
 pub mod pallet {
 	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
-	use pallet_nfts::MintWitness;
+	use pallet_nfts::{
+		CancelAttributesApprovalWitness, CollectionConfig, CollectionSettings, DestroyWitness,
+		MintSettings, MintWitness,
+	};
 	use sp_std::vec::Vec;
 	use types::{
 		AccountIdOf, AttributeNamespaceOf, BalanceOf, CollectionDetailsFor, CollectionIdOf,
-		ItemDetailsFor, ItemIdOf, ItemPriceOf, NftsOf, NftsWeightInfoOf,
+		CreateCollectionConfigFor, ItemDetailsFor, ItemIdOf, ItemPriceOf, NftsOf, NftsWeightInfoOf,
 	};
 
 	use super::*;
@@ -29,24 +32,12 @@ pub mod pallet {
 	#[repr(u8)]
 	#[allow(clippy::unnecessary_cast)]
 	pub enum Read<T: Config> {
-		/// Returns the owner of an item.
-		#[codec(index = 0)]
-		OwnerOf { collection: CollectionIdOf<T>, item: ItemIdOf<T> },
-		/// Returns the owner of a collection.
-		#[codec(index = 1)]
-		CollectionOwner(CollectionIdOf<T>),
 		/// Number of items existing in a concrete collection.
 		#[codec(index = 2)]
 		TotalSupply(CollectionIdOf<T>),
 		/// Returns the total number of items in the collection owned by the account.
 		#[codec(index = 3)]
 		BalanceOf { collection: CollectionIdOf<T>, owner: AccountIdOf<T> },
-		/// Returns the details of a collection.
-		#[codec(index = 4)]
-		Collection(CollectionIdOf<T>),
-		/// Returns the details of an item.
-		#[codec(index = 5)]
-		Item { collection: CollectionIdOf<T>, item: ItemIdOf<T> },
 		/// Whether a spender is allowed to transfer an item or items from owner.
 		#[codec(index = 6)]
 		Allowance {
@@ -55,6 +46,9 @@ pub mod pallet {
 			operator: AccountIdOf<T>,
 			item: Option<ItemIdOf<T>>,
 		},
+		/// Returns the owner of an item.
+		#[codec(index = 0)]
+		OwnerOf { collection: CollectionIdOf<T>, item: ItemIdOf<T> },
 		/// Returns the attribute of `item` for the given `key`.
 		#[codec(index = 6)]
 		GetAttribute {
@@ -63,20 +57,29 @@ pub mod pallet {
 			namespace: AttributeNamespaceOf<T>,
 			key: BoundedVec<u8, T::KeyLimit>,
 		},
+		/// Returns the owner of a collection.
+		#[codec(index = 1)]
+		CollectionOwner(CollectionIdOf<T>),
+		/// Returns the details of a collection.
+		#[codec(index = 4)]
+		Collection(CollectionIdOf<T>),
+		/// Returns the details of an item.
+		#[codec(index = 5)]
+		Item { collection: CollectionIdOf<T>, item: ItemIdOf<T> },
 	}
 
 	/// Results of state reads for the non-fungibles API.
 	#[derive(Debug)]
 	#[cfg_attr(feature = "std", derive(PartialEq, Clone))]
 	pub enum ReadResult<T: Config> {
-		OwnerOf(Option<AccountIdOf<T>>),
-		CollectionOwner(Option<AccountIdOf<T>>),
 		TotalSupply(u128),
 		BalanceOf(u32),
+		Allowance(bool),
+		OwnerOf(Option<AccountIdOf<T>>),
+		GetAttribute(Option<BoundedVec<u8, T::ValueLimit>>),
+		CollectionOwner(Option<AccountIdOf<T>>),
 		Collection(Option<CollectionDetailsFor<T>>),
 		Item(Option<ItemDetailsFor<T>>),
-		Allowance(bool),
-		GetAttribute(Option<BoundedVec<u8, T::ValueLimit>>),
 	}
 
 	impl<T: Config> ReadResult<T> {
@@ -141,19 +144,6 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		#[pallet::call_index(20)]
-		#[pallet::weight(NftsWeightInfoOf::<T>::create())]
-		pub fn create(_origin: OriginFor<T>) -> DispatchResult {
-			Ok(())
-		}
-
-		// TODO: Fix weight
-		#[pallet::call_index(21)]
-		#[pallet::weight(NftsWeightInfoOf::<T>::create())]
-		pub fn destroy(_origin: OriginFor<T>) -> DispatchResult {
-			Ok(())
-		}
-
 		#[pallet::call_index(0)]
 		#[pallet::weight(NftsWeightInfoOf::<T>::mint())]
 		pub fn mint(
@@ -250,6 +240,131 @@ pub mod pallet {
 			Self::deposit_event(Event::Approval { collection, item, operator, owner, approved });
 			Ok(())
 		}
+
+		#[pallet::call_index(4)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::create())]
+		pub fn create(
+			origin: OriginFor<T>,
+			admin: AccountIdOf<T>,
+			config: CreateCollectionConfigFor<T>,
+		) -> DispatchResult {
+			let collection_config = CollectionConfig {
+				settings: CollectionSettings::all_enabled(),
+				max_supply: config.max_supply,
+				mint_settings: MintSettings {
+					mint_type: config.mint_type,
+					start_block: config.start_block,
+					end_block: config.end_block,
+					..MintSettings::default()
+				},
+			};
+			NftsOf::<T>::create(origin, T::Lookup::unlookup(admin.clone()), collection_config)?;
+			Ok(())
+		}
+
+		#[pallet::call_index(5)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::destroy(
+    		witness.item_metadatas,
+    		witness.item_configs,
+    		witness.attributes,
+		))]
+		pub fn destroy(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			witness: DestroyWitness,
+		) -> DispatchResultWithPostInfo {
+			NftsOf::<T>::destroy(origin, collection, witness)
+		}
+
+		#[pallet::call_index(6)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::set_attribute())]
+		pub fn set_attribute(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: Option<ItemIdOf<T>>,
+			namespace: AttributeNamespaceOf<T>,
+			key: BoundedVec<u8, T::KeyLimit>,
+			value: BoundedVec<u8, T::ValueLimit>,
+		) -> DispatchResult {
+			NftsOf::<T>::set_attribute(origin, collection, item, namespace, key, value)
+		}
+
+		#[pallet::call_index(7)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::clear_attribute())]
+		pub fn clear_attribute(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: Option<ItemIdOf<T>>,
+			namespace: AttributeNamespaceOf<T>,
+			key: BoundedVec<u8, T::KeyLimit>,
+		) -> DispatchResult {
+			NftsOf::<T>::clear_attribute(origin, collection, item, namespace, key)
+		}
+
+		#[pallet::call_index(8)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::set_metadata())]
+		pub fn set_metadata(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+			data: BoundedVec<u8, T::StringLimit>,
+		) -> DispatchResult {
+			NftsOf::<T>::set_metadata(origin, collection, item, data)
+		}
+
+		#[pallet::call_index(9)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::clear_metadata())]
+		pub fn clear_metadata(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+		) -> DispatchResult {
+			NftsOf::<T>::clear_metadata(origin, collection, item)
+		}
+
+		#[pallet::call_index(10)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::approve_item_attributes())]
+		pub fn approve_item_attributes(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+			delegate: AccountIdOf<T>,
+		) -> DispatchResult {
+			NftsOf::<T>::approve_item_attributes(
+				origin,
+				collection,
+				item,
+				T::Lookup::unlookup(delegate.clone()),
+			)
+		}
+
+		#[pallet::call_index(11)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::cancel_item_attributes_approval(witness.account_attributes))]
+		pub fn cancel_item_attributes_approval(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			item: ItemIdOf<T>,
+			delegate: AccountIdOf<T>,
+			witness: CancelAttributesApprovalWitness,
+		) -> DispatchResult {
+			NftsOf::<T>::cancel_item_attributes_approval(
+				origin,
+				collection,
+				item,
+				T::Lookup::unlookup(delegate.clone()),
+				witness,
+			)
+		}
+
+		#[pallet::call_index(12)]
+		#[pallet::weight(NftsWeightInfoOf::<T>::set_collection_max_supply())]
+		pub fn set_max_supply(
+			origin: OriginFor<T>,
+			collection: CollectionIdOf<T>,
+			max_supply: u32,
+		) -> DispatchResult {
+			NftsOf::<T>::set_collection_max_supply(origin, collection, max_supply)
+		}
 	}
 
 	impl<T: Config> crate::Read for Pallet<T> {
@@ -274,27 +389,26 @@ pub mod pallet {
 		fn read(value: Self::Read) -> Self::Result {
 			use Read::*;
 			match value {
-				OwnerOf { collection, item } =>
-					ReadResult::OwnerOf(NftsOf::<T>::owner(collection, item)),
-				CollectionOwner(collection) =>
-					ReadResult::CollectionOwner(NftsOf::<T>::collection_owner(collection)),
 				TotalSupply(collection) => ReadResult::TotalSupply(
 					NftsOf::<T>::collection_items(collection).unwrap_or_default().into(),
 				),
-				Collection(collection) =>
-					ReadResult::Collection(pallet_nfts::Collection::<T>::get(collection)),
-				Item { collection, item } =>
-					ReadResult::Item(pallet_nfts::Item::<T>::get(collection, item)),
+				BalanceOf { collection, owner } =>
+					ReadResult::BalanceOf(pallet_nfts::AccountBalance::<T>::get(collection, owner)),
 				Allowance { collection, owner, operator, item } => ReadResult::Allowance(
 					NftsOf::<T>::check_allowance(&collection, &item, &owner, &operator).is_ok(),
 				),
-				BalanceOf { collection, owner } => ReadResult::BalanceOf(
-					pallet_nfts::AccountBalance::<T>::get((collection, owner)),
-				),
+				OwnerOf { collection, item } =>
+					ReadResult::OwnerOf(NftsOf::<T>::owner(collection, item)),
 				GetAttribute { collection, item, namespace, key } => ReadResult::GetAttribute(
 					pallet_nfts::Attribute::<T>::get((collection, item, namespace, key))
 						.map(|attribute| attribute.0),
 				),
+				CollectionOwner(collection) =>
+					ReadResult::CollectionOwner(NftsOf::<T>::collection_owner(collection)),
+				Collection(collection) =>
+					ReadResult::Collection(pallet_nfts::Collection::<T>::get(collection)),
+				Item { collection, item } =>
+					ReadResult::Item(pallet_nfts::Item::<T>::get(collection, item)),
 			}
 		}
 	}
