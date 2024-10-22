f4d1ba462feb85e5042780370627eae2d566db4f
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-22 14:15:10 +0700

    feat(nonfungibles): pop-api

--- pop-api/src/v0/nonfungibles/types.rs
+++ pop-api/src/v0/nonfungibles/types.rs
@@ -2,14 +2,34 @@ use super::*;
 use crate::primitives::AccountId;
 
 pub type ItemId = u32;
-pub type Collection = u32;
+pub type CollectionId = u32;
+pub type Balance = u32;
+
+/// Information about a collection.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct CollectionDetails {
+	/// Collection's owner.
+	pub owner: AccountId,
+	/// The total balance deposited by the owner for all the storage data associated with this
+	/// collection. Used by `destroy`.
+	pub owner_deposit: Balance,
+	/// The total number of outstanding items of this collection.
+	pub items: u32,
+	/// The total number of outstanding item metadata of this collection.
+	pub item_metadatas: u32,
+	/// The total number of outstanding item configs of this collection.
+	pub item_configs: u32,
+	/// The total number of attributes for this collection.
+	pub attributes: u32,
+}
 
 #[derive(Debug, PartialEq, Eq)]
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub struct CreateCollectionConfig<Price, BlockNumber, CollectionId> {
+pub struct CreateCollectionConfig {
 	pub max_supply: Option<u32>,
 	pub mint_type: MintType<CollectionId>,
-	pub price: Option<Price>,
+	pub price: Option<Balance>,
 	pub start_block: Option<BlockNumber>,
 	pub end_block: Option<BlockNumber>,
 }
@@ -38,4 +58,26 @@ pub enum MintType<CollectionId> {
 	Public,
 	/// Only holders of items in specified collection could mint new items.
 	HolderOf(CollectionId),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct CancelAttributesApprovalWitness {
+	/// An amount of attributes previously created by account.
+	pub account_attributes: u32,
+}
+
+/// Witness data for the destroy transactions.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct DestroyWitness {
+	/// The total number of items in this collection that have outstanding item metadata.
+	#[codec(compact)]
+	pub item_metadatas: u32,
+	/// The total number of outstanding item configs of this collection.
+	#[codec(compact)]
+	pub item_configs: u32,
+	/// The total number of attributes for this collection.
+	#[codec(compact)]
+	pub attributes: u32,
 }
