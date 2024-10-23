690ccedba7aced1741d948955b155f9cca6013d6
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2024-10-23 22:36:26 +0200

    missing SPONSORSHIPS in error tests

--- pop-api/src/v0/sponsorships/errors.rs
+++ pop-api/src/v0/sponsorships/errors.rs
@@ -1,100 +1,104 @@
 /// Represents various errors related to sponsorships.
 ///
-/// The `SponsorshipsError` provides a detailed and specific set of error types that can occur when
-/// interacting with sponsorships. Each variant signifies a particular error
+/// The `SponsorshipsError` provides a detailed and specific set of error types that can occur
+/// when interacting with sponsorships. Each variant signifies a particular error
 /// condition, facilitating precise error handling and debugging.
 ///
 /// The `Other` variant serves as a catch-all for any unexpected errors. For more
-/// detailed debugging, the `Other` variant can be converted into the richer `Error` type defined in
-/// the primitives crate.
-
+/// detailed debugging, the `Other` variant can be converted into the richer `Error` type
+/// defined in the primitives crate.
 use super::*;
 
 #[derive(Debug, PartialEq, Eq)]
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
 pub enum SponsorshipsError {
-    /// An unspecified or unknown error occurred.
-    Other(StatusCode),
-    /// This action cannot be sponsored.
-    CantSponsor,
-    /// The cost is higher than the max sponsored.
-    SponsorshipOutOfLimit,
+	/// An unspecified or unknown error occurred.
+	Other(StatusCode),
+	/// This action cannot be sponsored.
+	CantSponsor,
+	/// The cost is higher than the max sponsored.
+	SponsorshipOutOfLimit,
 }
 
 impl From<StatusCode> for SponsorshipsError {
-    /// Converts a `StatusCode` to a `SponsorshipsError`.
-    ///
-    /// This conversion maps a `StatusCode`, returned by the runtime, to a more descriptive
-    /// `SponsorshipsError`. This provides better context and understanding of the error, allowing
-    /// developers to handle the most important errors effectively.
-    fn from(value: StatusCode) -> Self {
-        let encoded = value.0.to_le_bytes();
-        match encoded {
-            [_, SPONSORSHIPS, 0, _] => SponsorshipsError::CantSponsor,
-            [_, SPONSORSHIPS, 1, _] => SponsorshipsError::SponsorshipOutOfLimit,
-            _ => SponsorshipsError::Other(value),
-        }
-    }
+	/// Converts a `StatusCode` to a `SponsorshipsError`.
+	///
+	/// This conversion maps a `StatusCode`, returned by the runtime, to a more descriptive
+	/// `SponsorshipsError`. This provides better context and understanding of the error, allowing
+	/// developers to handle the most important errors effectively.
+	fn from(value: StatusCode) -> Self {
+		let encoded = value.0.to_le_bytes();
+		match encoded {
+			[_, SPONSORSHIPS, 0, _] => SponsorshipsError::CantSponsor,
+			[_, SPONSORSHIPS, 1, _] => SponsorshipsError::SponsorshipOutOfLimit,
+			_ => SponsorshipsError::Other(value),
+		}
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::SponsorshipsError;
-    use crate::{
-        primitives::{
-            Error::{self, *}
-        },
-        StatusCode,
-    };
-    use ink::scale::{Decode, Encode};
+	use ink::scale::Encode;
 
-    fn error_into_status_code(error: Error) -> StatusCode {
-        let mut encoded_error = error.encode();
-        encoded_error.resize(4, 0);
-        let value = u32::from_le_bytes(
-            encoded_error.try_into().expect("qed, resized to 4 bytes line above"),
-        );
-        value.into()
-    }
+	use super::SponsorshipsError;
+	use crate::{
+		constants::SPONSORSHIPS,
+		primitives::{
+			ArithmeticError::*,
+			Error::{self, *},
+			TokenError::*,
+			TransactionalError::*,
+		},
+		StatusCode,
+	};
 
-    fn into_error<T: From<StatusCode>>(error: Error) -> T {
-        error_into_status_code(error).into()
-    }
+	fn error_into_status_code(error: Error) -> StatusCode {
+		let mut encoded_error = error.encode();
+		encoded_error.resize(4, 0);
+		let value = u32::from_le_bytes(
+			encoded_error.try_into().expect("qed, resized to 4 bytes line above"),
+		);
+		value.into()
+	}
 
-    #[test]
-    fn converting_status_code_into_sponsorships_error_works() {
-        let other_errors = vec![
-            Other,
-            CannotLookup,
-            BadOrigin,
-            // `ModuleError` other than assets module.
-            Module { index: 2, error: [5, 0] },
-            ConsumerRemaining,
-            NoProviders,
-            TooManyConsumers,
-            Token(OnlyProvider),
-            Arithmetic(Overflow),
-            Transactional(NoLayer),
-            Exhausted,
-            Corruption,
-            Unavailable,
-            RootNotAllowed,
-            Unknown { dispatch_error_index: 5, error_index: 5, error: 1 },
-            DecodingFailed,
-        ];
-        for error in other_errors {
-            let status_code: StatusCode = error_into_status_code(error);
-            let fungibles_error: SponsorshipsError = status_code.into();
-            assert_eq!(fungibles_error, SponsorshipsError::Other(status_code))
-        }
+	fn into_error<T: From<StatusCode>>(error: Error) -> T {
+		error_into_status_code(error).into()
+	}
 
-        assert_eq!(
-            into_error::<SponsorshipsError>(Module { index: SPONSORSHIPS, error: [0, 0] }),
-            SponsorshipsError::CantSponsor
-        );
-        assert_eq!(
-            into_error::<SponsorshipsError>(Module { index: SPONSORSHIPS, error: [1, 0] }),
-            SponsorshipsError::SponsorshipOutOfLimit
-        );
-    }
+	#[test]
+	fn converting_status_code_into_sponsorships_error_works() {
+		let other_errors = vec![
+			Other,
+			CannotLookup,
+			BadOrigin,
+			// `ModuleError` other than assets module.
+			Module { index: 2, error: [5, 0] },
+			ConsumerRemaining,
+			NoProviders,
+			TooManyConsumers,
+			Token(OnlyProvider),
+			Arithmetic(Overflow),
+			Transactional(NoLayer),
+			Exhausted,
+			Corruption,
+			Unavailable,
+			RootNotAllowed,
+			Unknown { dispatch_error_index: 5, error_index: 5, error: 1 },
+			DecodingFailed,
+		];
+		for error in other_errors {
+			let status_code: StatusCode = error_into_status_code(error);
+			let fungibles_error: SponsorshipsError = status_code.into();
+			assert_eq!(fungibles_error, SponsorshipsError::Other(status_code))
+		}
+
+		assert_eq!(
+			into_error::<SponsorshipsError>(Module { index: SPONSORSHIPS, error: [0, 0] }),
+			SponsorshipsError::CantSponsor
+		);
+		assert_eq!(
+			into_error::<SponsorshipsError>(Module { index: SPONSORSHIPS, error: [1, 0] }),
+			SponsorshipsError::SponsorshipOutOfLimit
+		);
+	}
 }
--- pop-api/src/v0/sponsorships/traits.rs
+++ pop-api/src/v0/sponsorships/traits.rs
@@ -5,17 +5,17 @@ use super::*;
 /// The CanSponsor trait.
 #[ink::trait_definition]
 pub trait CanSponsor {
-    /// Registers a new sponsorship relation between the caller and an account.
-    ///
-    /// # Parameters
-    /// - `beneficiary` - The account to be sponsored.
-    #[ink(message)]
-    fn sponsor_account(&self, beneficiary: AccountId) -> Result<()>;
+	/// Registers a new sponsorship relation between the caller and an account.
+	///
+	/// # Parameters
+	/// - `beneficiary` - The account to be sponsored.
+	#[ink(message)]
+	fn sponsor_account(&self, beneficiary: AccountId) -> Result<()>;
 
-    /// Remove an account from the list of sponsored accounts managed by origin.
-    ///
-    /// Parameters
-    /// - `account`: Account to be removed from a sponsorship.
-    #[ink(message)]
-    fn remove_sponsorship(&self, account: AccountId) -> Result<()>;
+	/// Remove an account from the list of sponsored accounts managed by origin.
+	///
+	/// Parameters
+	/// - `account`: Account to be removed from a sponsorship.
+	#[ink(message)]
+	fn remove_sponsorship(&self, account: AccountId) -> Result<()>;
 }
