825a36846a0b0ee602e8986c31187c26052dd056
Frank Bell <frank@r0gue.io>
2024-10-24 13:36:41 +0100

    test(revive): environment integration tests

--- pop-api/src/primitives.rs
+++ pop-api/src/primitives.rs
@@ -3,4 +3,6 @@ pub use pop_primitives::*;
 
 // Public due to integration tests crate.
 pub type AccountId = <DefaultEnvironment as Environment>::AccountId;
-pub(crate) type Balance = <DefaultEnvironment as Environment>::Balance;
+pub type Balance = <DefaultEnvironment as Environment>::Balance;
+pub type Hash = <DefaultEnvironment as Environment>::Hash;
+pub type Timestamp = <DefaultEnvironment as Environment>::Timestamp;
