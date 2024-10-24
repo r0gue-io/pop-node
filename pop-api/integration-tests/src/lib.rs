62006c23ad3ca27e8730cadfec3b694ca302f5d2
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-24 12:30:38 +0700

    test(nonfungibles): add more tests

--- pop-api/integration-tests/src/lib.rs
+++ pop-api/integration-tests/src/lib.rs
@@ -30,7 +30,7 @@ const ALICE: AccountId32 = AccountId32::new([1_u8; 32]);
 const BOB: AccountId32 = AccountId32::new([2_u8; 32]);
 const DEBUG_OUTPUT: pallet_revive::DebugInfo = pallet_revive::DebugInfo::UnsafeDebug;
 const FERDIE: AccountId32 = AccountId32::new([3_u8; 32]);
-const GAS_LIMIT: Weight = Weight::from_parts(100_000_000_000, 3 * 1024 * 1024);
+const GAS_LIMIT: Weight = Weight::from_parts(500_000_000_000, 3 * 1024 * 1024);
 const INIT_AMOUNT: Balance = 100_000_000 * UNIT;
 const INIT_VALUE: Balance = 100 * UNIT;
 
