1943669fd14a2d80ab2ff5241d2e5d0559674941
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-24 17:04:52 +0700

    chore: update weight pallet-nfts file

--- runtime/devnet/src/lib.rs
+++ runtime/devnet/src/lib.rs
@@ -647,6 +647,7 @@ mod benches {
 		[pallet_session, SessionBench::<Runtime>]
 		[pallet_timestamp, Timestamp]
 		[pallet_message_queue, MessageQueue]
+		[pallet_nfts, Nfts]
 		[pallet_sudo, Sudo]
 		[pallet_collator_selection, CollatorSelection]
 		[cumulus_pallet_parachain_system, ParachainSystem]
