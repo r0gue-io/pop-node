89e07e6a77c5ba6aacfbd27b3a879ccbbbc090ab
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-31 09:50:15 +0700

    fix: comments

--- pop-api/examples/fungibles/traits.rs
+++ pop-api/examples/fungibles/traits.rs
@@ -3,14 +3,23 @@ use super::*;
 #[ink::trait_definition]
 pub trait MinterRole {
 	/// Check if the caller is the minter of the contract.
+	///
+	/// # Parameters
+	/// - `minter` - The minter account.
 	#[ink(message)]
 	fn ensure_minter(&self, account: AccountId) -> Result<(), Psp22Error>;
 
 	/// Add a new minter by existing minters.
+	///
+	/// # Parameters
+	/// - `minter` - The new minter account.
 	#[ink(message)]
 	fn add_minter(&mut self, minter: AccountId) -> Result<(), Psp22Error>;
 
-	/// Add a new minter by existing minters.
+	/// Remove a minter by existing minters.
+	///
+	/// # Parameters
+	/// - `minter` - The removed minter account.
 	#[ink(message)]
 	fn remove_minter(&mut self, minter: AccountId) -> Result<(), Psp22Error>;
 }
