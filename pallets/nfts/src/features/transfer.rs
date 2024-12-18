eadffa538c74b4650d1853b8a518d13530d82699
Frank Bell <frank@r0gue.io>
2024-12-18 08:01:08 +0000

    refactor: account balance deposit

--- pallets/nfts/src/features/transfer.rs
+++ pallets/nfts/src/features/transfer.rs
@@ -19,6 +19,7 @@
 //! of the NFTs pallet.
 
 use frame_support::pallet_prelude::*;
+use sp_runtime::ArithmeticError;
 
 use crate::*;
 
@@ -87,16 +88,57 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 		with_details(&collection_details, &mut details)?;
 
 		// Update account balance of the owner.
-		let balance = AccountBalance::<T, I>::take(collection, &details.owner)
-			.checked_sub(1)
-			.ok_or(Error::<T, I>::NoItemOwned)?;
-		if balance > 0 {
-			AccountBalance::<T, I>::insert(collection, &details.owner, balance);
-		}
+		AccountBalance::<T, I>::try_mutate_exists(
+			collection,
+			&details.owner,
+			|maybe_balance| -> DispatchResult {
+				// NOTE: same code as do_burn implementation - should be refactored into a
+				// reusable function for both burn and the source part of the transfer
+				match maybe_balance {
+					None => return Err(Error::<T, I>::NoItemOwned.into()),
+					Some((mut balance, (depositor_account, deposit_amount))) => {
+						balance = balance.checked_sub(1).ok_or(ArithmeticError::Underflow)?;
+						if balance == 0 {
+							T::Currency::unreserve(&depositor_account, *deposit_amount);
+							*maybe_balance = None;
+						}
+						Ok(())
+					},
+				}
+			},
+		)?;
 		// Update account balance of the destination account.
-		AccountBalance::<T, I>::mutate(collection, &dest, |balance| {
-			balance.saturating_inc();
-		});
+		AccountBalance::<T, I>::mutate(collection, &dest, |maybe_balance| -> DispatchResult {
+			// NOTE: very similar code to do_mint implementation - should be refactored into a
+			// reusable function for both minting and the dest part of the transfer
+			match maybe_balance {
+				None => {
+					let deposit_account = match frame_system::Pallet::<T>::account_exists(&dest) {
+						// NOTE: no balance check, would fail in reservation below if dest has
+						// insufficient funds. As it is just a transfer and not a mint, could caller
+						// just resubmit once dest balance rectified?
+						true => dest.clone(),
+						// TODO: replace with signed origin account, which requires a parameter to
+						// be added to the function
+						false => collection_details.owner,
+					};
+					// This is questionable - should collection config be able to override
+					// chain security?
+					let deposit_amount = match collection_config
+						.is_setting_enabled(CollectionSetting::DepositRequired)
+					{
+						true => T::BalanceDeposit::get(),
+						false => Zero::zero(),
+					};
+					T::Currency::reserve(&deposit_account, deposit_amount)?;
+					*maybe_balance = Some((1, (deposit_account, deposit_amount)));
+				},
+				Some((balance, _deposit)) => {
+					balance.saturating_inc();
+				},
+			}
+			Ok(())
+		})?;
 
 		// Update account ownership information.
 		Account::<T, I>::remove((&details.owner, &collection, &item));
