e716881363b874560eba213eff55b719d90dbb7e
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-01-25 00:19:32 +0100

    style(collation): order tests following pallet config

--- runtime/mainnet/src/config/collation.rs
+++ runtime/mainnet/src/config/collation.rs
@@ -74,6 +74,14 @@ mod tests {
 	use super::*;
 
 	#[test]
+	fn authorship_notes_block_author_via_collator_selection() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_authorship::Config>::EventHandler>(),
+			TypeId::of::<(CollatorSelection,)>(),
+		);
+	}
+
+	#[test]
 	fn authorship_finds_block_author_via_index_from_digests_within_block_header() {
 		assert_eq!(
 			TypeId::of::<<Runtime as pallet_authorship::Config>::FindAuthor>(),
@@ -82,11 +90,37 @@ mod tests {
 	}
 
 	#[test]
-	fn authorship_notes_block_author_via_collator_selection() {
+	fn aura_allows_multiple_blocks_per_slot() {
 		assert_eq!(
-			TypeId::of::<<Runtime as pallet_authorship::Config>::EventHandler>(),
-			TypeId::of::<(CollatorSelection,)>(),
+			TypeId::of::<<Runtime as pallet_aura::Config>::AllowMultipleBlocksPerSlot>(),
+			TypeId::of::<ConstBool<true>>(),
 		);
+	}
+
+	#[test]
+	fn aura_uses_sr25519_for_authority_id() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_aura::Config>::AuthorityId>(),
+			TypeId::of::<sp_consensus_aura::sr25519::AuthorityId>(),
+		);
+	}
+
+	#[test]
+	fn aura_disabled_validators_not_used() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_aura::Config>::DisabledValidators>(),
+			TypeId::of::<()>(),
+		);
+	}
+
+	#[test]
+	fn aura_max_authorities() {
+		assert_eq!(<<Runtime as pallet_aura::Config>::MaxAuthorities as Get<u32>>::get(), 3_600);
+	}
+
+	#[test]
+	fn aura_has_six_second_blocks() {
+		assert_eq!(<<Runtime as pallet_aura::Config>::SlotDuration as Get<u64>>::get(), 6_000);
 	}
 
 	#[test]
@@ -98,10 +132,38 @@ mod tests {
 	}
 
 	#[test]
-	fn collator_selection_update_origin_limited_to_root() {
+	fn collator_selection_kick_threshold_matches_period() {
 		assert_eq!(
-			TypeId::of::<<Runtime as pallet_collator_selection::Config>::UpdateOrigin>(),
-			TypeId::of::<EnsureRoot<<Runtime as frame_system::Config>::AccountId>>(),
+			<<Runtime as pallet_collator_selection::Config>::KickThreshold as Get<u32>>::get(),
+			Period::get(),
+		);
+	}
+
+	#[test]
+	fn collator_selection_candidates_disabled() {
+		// Disabled to start until sufficient distribution/value to allow candidates to provide
+		// candidacy bond
+		assert_eq!(
+			<<Runtime as pallet_collator_selection::Config>::MaxCandidates as Get<u32>>::get(),
+			0
+		);
+	}
+
+	#[test]
+	fn collator_selection_allows_max_twenty_invulnerables() {
+		// Additional invulnerables can be added after genesis via `UpdateOrigin`
+		assert_eq!(
+			<<Runtime as pallet_collator_selection::Config>::MaxInvulnerables as Get<u32>>::get(),
+			20
+		);
+	}
+
+	#[test]
+	fn collator_selection_requires_at_least_three_collators() {
+		assert_eq!(
+			<<Runtime as pallet_collator_selection::Config>::MinEligibleCollators as Get<u32>>::get(
+			),
+			3
 		);
 	}
 
@@ -125,30 +187,10 @@ mod tests {
 	}
 
 	#[test]
-	fn collator_selection_candidates_disabled() {
-		// Disabled to start until sufficient distribution/value to allow candidates to provide
-		// candidacy bond
+	fn collator_selection_update_origin_limited_to_root() {
 		assert_eq!(
-			<<Runtime as pallet_collator_selection::Config>::MaxCandidates as Get<u32>>::get(),
-			0
-		);
-	}
-
-	#[test]
-	fn collator_selection_requires_at_least_five_collators() {
-		assert_eq!(
-			<<Runtime as pallet_collator_selection::Config>::MinEligibleCollators as Get<u32>>::get(
-			),
-			3
-		);
-	}
-
-	#[test]
-	fn collator_selection_allows_max_twenty_invulnerables() {
-		// Additional invulnerables can be added after genesis via `UpdateOrigin`
-		assert_eq!(
-			<<Runtime as pallet_collator_selection::Config>::MaxInvulnerables as Get<u32>>::get(),
-			20
+			TypeId::of::<<Runtime as pallet_collator_selection::Config>::UpdateOrigin>(),
+			TypeId::of::<EnsureRoot<<Runtime as frame_system::Config>::AccountId>>(),
 		);
 	}
 
@@ -181,49 +223,6 @@ mod tests {
 	}
 
 	#[test]
-	fn session_identifies_collators_using_account_id() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::ValidatorId>(),
-			TypeId::of::<<Runtime as frame_system::Config>::AccountId>(),
-		);
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::ValidatorIdOf>(),
-			TypeId::of::<pallet_collator_selection::IdentityCollator>(),
-		);
-	}
-
-	#[test]
-	fn session_length_is_predefined_period_of_blocks() {
-		assert_eq!(Period::get(), 6 * HOURS);
-		assert_eq!(Period::get(), (60 / 6) * 60 * 6); // 6s blocks per minute * minutes in an hour * hours
-		let periodic_sessions = TypeId::of::<pallet_session::PeriodicSessions<Period, Offset>>();
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::ShouldEndSession>(),
-			periodic_sessions,
-		);
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::NextSessionRotation>(),
-			periodic_sessions,
-		);
-	}
-
-	#[test]
-	fn session_collators_managed_by_collator_selection() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::SessionManager>(),
-			TypeId::of::<CollatorSelection>(),
-		);
-	}
-
-	#[test]
-	fn session_handled_by_aura() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_session::Config>::SessionHandler>(),
-			TypeId::of::<(Aura,)>(),
-		);
-	}
-
-	#[test]
 	fn session_keys_provided_by_aura() {
 		assert_eq!(
 			TypeId::of::<<Runtime as pallet_session::Config>::Keys>(),
@@ -236,44 +235,54 @@ mod tests {
 	}
 
 	#[test]
+	fn session_length_is_predefined_period_of_blocks() {
+		assert_eq!(Period::get(), 6 * HOURS);
+		assert_eq!(Period::get(), (60 / 6) * 60 * 6); // 6s blocks per minute * minutes in an hour * hours
+		let periodic_sessions = TypeId::of::<pallet_session::PeriodicSessions<Period, Offset>>();
+
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::NextSessionRotation>(),
+			periodic_sessions,
+		);
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::ShouldEndSession>(),
+			periodic_sessions,
+		);
+	}
+
+	#[test]
+	fn session_handled_by_aura() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::SessionHandler>(),
+			TypeId::of::<(Aura,)>(),
+		);
+	}
+
+	#[test]
+	fn session_collators_managed_by_collator_selection() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::SessionManager>(),
+			TypeId::of::<CollatorSelection>(),
+		);
+	}
+
+	#[test]
+	fn session_identifies_collators_using_account_id() {
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::ValidatorId>(),
+			TypeId::of::<<Runtime as frame_system::Config>::AccountId>(),
+		);
+		assert_eq!(
+			TypeId::of::<<Runtime as pallet_session::Config>::ValidatorIdOf>(),
+			TypeId::of::<pallet_collator_selection::IdentityCollator>(),
+		);
+	}
+
+	#[test]
 	fn session_does_not_use_default_weights() {
 		assert_ne!(
 			TypeId::of::<<Runtime as pallet_session::Config>::WeightInfo>(),
 			TypeId::of::<()>(),
 		);
-	}
-
-	#[test]
-	fn aura_uses_sr25519_for_authority_id() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_aura::Config>::AuthorityId>(),
-			TypeId::of::<sp_consensus_aura::sr25519::AuthorityId>(),
-		);
-	}
-
-	#[test]
-	fn aura_max_authorities() {
-		assert_eq!(<<Runtime as pallet_aura::Config>::MaxAuthorities as Get<u32>>::get(), 3_600);
-	}
-
-	#[test]
-	fn aura_disabled_validators_not_used() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_aura::Config>::DisabledValidators>(),
-			TypeId::of::<()>(),
-		);
-	}
-
-	#[test]
-	fn aura_allows_multiple_blocks_per_slot() {
-		assert_eq!(
-			TypeId::of::<<Runtime as pallet_aura::Config>::AllowMultipleBlocksPerSlot>(),
-			TypeId::of::<ConstBool<true>>(),
-		);
-	}
-
-	#[test]
-	fn aura_has_six_second_blocks() {
-		assert_eq!(<<Runtime as pallet_aura::Config>::SlotDuration as Get<u64>>::get(), 6_000);
 	}
 }
