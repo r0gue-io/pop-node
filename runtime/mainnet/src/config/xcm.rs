990c62047ba0e6dfb6187ab240c68e76c99f97d4
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-01-31 02:22:08 +0100

    fix(xcm): typo
    fix(xcm): XcmReserveTransferFilter is Everything

--- runtime/mainnet/src/config/xcm.rs
+++ runtime/mainnet/src/config/xcm.rs
@@ -693,7 +693,7 @@ mod tests {
 	#[test]
 	fn pallet_xcm_admin_origin_ensures_root() {
 		assert_eq!(
-			TypeId::of::<<XcmConfig as pallet_xcm::Config>::AdminOrigin>(),
+			TypeId::of::<<Runtime as pallet_xcm::Config>::AdminOrigin>(),
 			TypeId::of::<EnsureRoot<AccountId>>(),
 		);
 	}
@@ -701,7 +701,7 @@ mod tests {
 	#[test]
 	fn pallet_xcm_advertises_current_xcm_version() {
 		assert_eq!(
-			TypeId::of::<<XcmConfig as pallet_xcm::Config>::AdvertisedXcmVersion>(),
+			TypeId::of::<<Runtime as pallet_xcm::Config>::AdvertisedXcmVersion>(),
 			TypeId::of::<pallet_xcm::CurrentXcmVersion>(),
 		);
 	}
@@ -831,7 +831,7 @@ mod tests {
 	fn pallet_xcm_reserve_transfer_filter_only_allows_dot_from_ah() {
 		assert_eq!(
 			TypeId::of::<<Runtime as pallet_xcm::Config>::XcmReserveTransferFilter>(),
-			TypeId::of::<NativeAssetFrom<AssetHub>>(),
+			TypeId::of::<Everything>(),
 		);
 	}
 
