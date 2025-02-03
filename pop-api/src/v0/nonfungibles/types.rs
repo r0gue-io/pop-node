8b516d7b7633782e7970c1d4e919cc846b5734cb
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-02-03 17:41:45 +0700

    chore: update types

--- pop-api/src/v0/nonfungibles/types.rs
+++ pop-api/src/v0/nonfungibles/types.rs
@@ -1,4 +1,4 @@
-//! A set of data types used as returned types or parameter types of the non-fungibles API methods.
+//! A set of data types for use in smart contracts interacting with the non-fungibles API.
 
 use enumflags2::{bitflags, BitFlags};
 
@@ -7,7 +7,42 @@ use crate::{macros::impl_codec_bitflags, primitives::AccountId};
 
 pub type ItemId = u32;
 pub type CollectionId = u32;
-pub(super) type Balance = u32;
+type Balance = u32;
+
+/// Attribute namespaces for non-fungible tokens.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub enum AttributeNamespace {
+	/// An attribute set by collection's owner.
+	#[codec(index = 1)]
+	CollectionOwner,
+	/// An attribute set by item's owner.
+	#[codec(index = 2)]
+	ItemOwner,
+	/// An attribute set by a pre-approved account.
+	#[codec(index = 3)]
+	Account(AccountId),
+}
+
+/// A witness data to cancel attributes approval operation.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct CancelAttributesApprovalWitness {
+	/// An amount of attributes previously created by account.
+	pub account_attributes: u32,
+}
+
+/// Collection's configuration.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct CollectionConfig {
+	/// Collection's settings.
+	pub settings: CollectionSettings,
+	/// Collection's max supply.
+	pub max_supply: Option<u32>,
+	/// Default settings each item will get during the mint.
+	pub mint_settings: MintSettings,
+}
 
 /// Information about a collection.
 #[derive(Debug, PartialEq, Eq)]
@@ -26,80 +61,6 @@ pub struct CollectionDetails {
 	pub item_configs: u32,
 	/// The total number of attributes for this collection.
 	pub attributes: u32,
-}
-
-/// Attribute namespaces for non-fungible tokens.
-#[derive(Debug, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub enum AttributeNamespace {
-	/// An attribute set by collection's owner.
-	#[codec(index = 1)]
-	CollectionOwner,
-	/// An attribute set by item's owner.
-	#[codec(index = 2)]
-	ItemOwner,
-	/// An attribute set by a pre-approved account.
-	#[codec(index = 3)]
-	Account(AccountId),
-}
-
-/// Mint type. Can the NFT be create by anyone, or only the creator of the collection,
-/// or only by wallets that already hold an NFT from a certain collection?
-/// The ownership of a privately minted NFT is still publicly visible.
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub enum MintType {
-	/// Only an `Issuer` could mint items.
-	Issuer,
-	/// Anyone could mint items.
-	Public,
-	/// Only holders of items in specified collection could mint new items.
-	HolderOf(CollectionId),
-}
-
-/// A witness data to cancel attributes approval operation.
-#[derive(Debug, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub struct CancelAttributesApprovalWitness {
-	/// An amount of attributes previously created by account.
-	pub account_attributes: u32,
-}
-
-/// Witness data for the destroy transactions.
-#[derive(Debug, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub struct DestroyWitness {
-	/// The total number of items in this collection that have outstanding item metadata.
-	#[codec(compact)]
-	pub item_metadatas: u32,
-	/// The total number of outstanding item configs of this collection.
-	#[codec(compact)]
-	pub item_configs: u32,
-	/// The total number of attributes for this collection.
-	#[codec(compact)]
-	pub attributes: u32,
-}
-
-/// Witness data for items mint transactions.
-#[derive(Debug, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub struct MintWitness {
-	/// Provide the id of the item in a required collection.
-	pub owned_item: Option<ItemId>,
-	/// The price specified in mint settings.
-	pub mint_price: Option<Balance>,
-}
-
-/// Collection's configuration.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-#[ink::scale_derive(Encode, Decode, TypeInfo)]
-pub struct CollectionConfig {
-	/// Collection's settings.
-	pub settings: CollectionSettings,
-	/// Collection's max supply.
-	pub max_supply: Option<u32>,
-	/// Default settings each item will get during the mint.
-	pub mint_settings: MintSettings,
 }
 
 /// Support for up to 64 user-enabled features on a collection.
@@ -168,6 +129,21 @@ impl ItemSettings {
 
 impl_codec_bitflags!(ItemSettings, u64, ItemSetting);
 
+/// Witness data for the destroy transactions.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct DestroyWitness {
+	/// The total number of items in this collection that have outstanding item metadata.
+	#[codec(compact)]
+	pub item_metadatas: u32,
+	/// The total number of outstanding item configs of this collection.
+	#[codec(compact)]
+	pub item_configs: u32,
+	/// The total number of attributes for this collection.
+	#[codec(compact)]
+	pub attributes: u32,
+}
+
 /// Holds the information about minting.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
@@ -195,4 +171,28 @@ impl Default for MintSettings {
 			default_item_settings: ItemSettings::all_enabled(),
 		}
 	}
+}
+
+/// Mint type. Can the NFT be create by anyone, or only the creator of the collection,
+/// or only by wallets that already hold an NFT from a certain collection?
+/// The ownership of a privately minted NFT is still publicly visible.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub enum MintType {
+	/// Only an `Issuer` could mint items.
+	Issuer,
+	/// Anyone could mint items.
+	Public,
+	/// Only holders of items in specified collection could mint new items.
+	HolderOf(CollectionId),
+}
+
+/// Witness data for items mint transactions.
+#[derive(Debug, PartialEq, Eq)]
+#[ink::scale_derive(Encode, Decode, TypeInfo)]
+pub struct MintWitness {
+	/// Provide the id of the item in a required collection.
+	pub owned_item: Option<ItemId>,
+	/// The price specified in mint settings.
+	pub mint_price: Option<Balance>,
 }
