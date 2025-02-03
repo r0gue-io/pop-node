3a48998b050a9de17f7579a9a15aa88a4cd1a3b8
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-02-03 18:39:11 +0100

    docs(system): document public config modules

--- runtime/mainnet/src/config/mod.rs
+++ runtime/mainnet/src/config/mod.rs
@@ -1,5 +1,9 @@
+/// Governance configuration of pop.
 pub mod governance;
+/// Proxy configuration of pop.
 pub(crate) mod proxy;
+/// Configuration related to pop's system modules.
 pub mod system;
 mod utility;
+/// Cross chain configuration of pop.
 pub mod xcm;
