ed3e7c20d7e8de3d54cde8b31ca85bf7cb397a41
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-02-20 13:23:46 +0100

    feat(benchmarks): add every pallet benchmark to mainnet without XCM

--- runtime/mainnet/src/benchmarks.rs
+++ runtime/mainnet/src/benchmarks.rs
@@ -1,11 +1,40 @@
 frame_benchmarking::define_benchmarks!(
-		[frame_system, SystemBench::<Runtime>]
-		[pallet_balances, Balances]
-		[pallet_session, SessionBench::<Runtime>]
-		[pallet_timestamp, Timestamp]
-		[pallet_message_queue, MessageQueue]
-		[pallet_sudo, Sudo]
-		[pallet_collator_selection, CollatorSelection]
-		[cumulus_pallet_parachain_system, ParachainSystem]
-		[cumulus_pallet_xcmp_queue, XcmpQueue]
-	);
+	// Ordered as per runtime
+	// System
+	[frame_system, SystemBench::<Runtime>]
+	[frame_system_extensions, SystemExtensionsBench::<Runtime>]
+	[cumulus_pallet_parachain_system, ParachainSystem]
+	[pallet_timestamp, Timestamp]
+	// Monetary
+	[pallet_balances, Balances]
+	[pallet_transaction_payment, TransactionPayment]
+	[pallet_treasury, Treasury]
+	// Governance
+	[pallet_sudo, Sudo]
+	[pallet_collective, Council]
+	[pallet_motion, Motion]
+	// Collation support
+	[pallet_collator_selection, CollatorSelection]
+	[pallet_session, SessionBench::<Runtime>]
+	// Scheduler
+	[pallet_scheduler, Scheduler]
+	// Preimage
+	[pallet_preimage, Preimage]
+	// XCM
+	[cumulus_pallet_xcmp_queue, XcmpQueue]
+	// TODO: intro xcm benchmarks
+	//[pallet_xcm, PolkadotXcm::<Runtime>]
+	[pallet_message_queue, MessageQueue]
+	// Contracts
+	[pallet_revive, Revive]
+	// Proxy
+	[pallet_proxy, Proxy]
+	// Multisig
+	[pallet_multisig, Multisig]
+	// Utility
+	[pallet_utility, Utility]
+	// Assets
+	[pallet_nfts, Nfts]
+	// TODO: Name TrustBackedAssets
+	[pallet_assets, Assets]
+);
