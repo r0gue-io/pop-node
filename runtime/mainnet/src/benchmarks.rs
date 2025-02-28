fd64662c8c9cf31644a929bfb059caaec5bab52a
Alejandro Martinez Andres <11448715+al3mart@users.noreply.github.com>
2025-02-28 16:20:34 +0100

    refactor(benchmark): rename TrustBackedAssets to Assets

--- runtime/mainnet/src/benchmarks.rs
+++ runtime/mainnet/src/benchmarks.rs
@@ -1,8 +1,3 @@
-use crate::{config::assets::TrustBackedAssetsInstance, Runtime};
-
-/// Instance of `pallet_assets` used for benchmarks of trust backed assets.
-pub type TrustBackedAssets = pallet_assets::Pallet<Runtime, TrustBackedAssetsInstance>;
-
 frame_benchmarking::define_benchmarks!(
 	// Ordered as per runtime
 	// System
@@ -40,5 +35,5 @@ frame_benchmarking::define_benchmarks!(
 	[pallet_utility, Utility]
 	// Assets
 	[pallet_nfts, Nfts]
-	[pallet_assets, TrustBackedAssets]
+	[pallet_assets, Assets]
 );
