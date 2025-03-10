e193b0c34215f0ea16f606a5e8a14c89363cd2f5
f-gate <felixgate13@gmail.com>
2025-03-10 12:30:24 +0000

    comments

--- pallets/api/src/messaging/deposits.rs
+++ pallets/api/src/messaging/deposits.rs
@@ -15,27 +15,58 @@ pub fn calculate_protocol_deposit<T: Config, ByteFee: Get<BalanceOf<T>>>(
 ) -> BalanceOf<T> {
 	let base: usize = match p {
 		ProtocolStorageDeposit::XcmQueries =>
-			KeyLenOf::<XcmQueries<T>>::get() as usize +
-				AccountIdOf::<T>::max_encoded_len() +
-				MessageId::max_encoded_len() +
-				Option::<Callback<T::AccountId>>::max_encoded_len(),
-		ProtocolStorageDeposit::IsmpRequests =>
-			KeyLenOf::<IsmpRequests<T>>::get() as usize +
-				AccountIdOf::<T>::max_encoded_len() +
-				MessageId::max_encoded_len(),
+			(KeyLenOf::<XcmQueries<T>>::get() as usize)
+				.saturating_add(AccountIdOf::<T>::max_encoded_len())
+				.saturating_add(MessageId::max_encoded_len())
+				.saturating_add(Option::<Callback<T::AccountId>>::max_encoded_len()),
+
+		ProtocolStorageDeposit::IsmpRequests => 
+			(KeyLenOf::<IsmpRequests<T>>::get() as usize)
+				.saturating_add(AccountIdOf::<T>::max_encoded_len())
+				.saturating_add(MessageId::max_encoded_len()),
 	};
-	ByteFee::get() * base.saturated_into()
+	ByteFee::get().saturating_mul(base.saturated_into())
 }
 
 /// Calculate the deposit for the storage used for the Message enum.
 pub fn calculate_message_deposit<T: Config, ByteFee: Get<BalanceOf<T>>>() -> BalanceOf<T> {
-	ByteFee::get() *
+	ByteFee::get().saturating_mul(
 		(KeyLenOf::<Messages<T>>::get() as usize + Message::<T>::max_encoded_len())
-			.saturated_into()
+			.saturated_into())
 }
 
 /// Blanket implementation of generating the deposit for a type that implements MaxEncodedLen.
 pub fn calculate_deposit_of<T: Config, ByteFee: Get<BalanceOf<T>>, U: MaxEncodedLen>(
 ) -> BalanceOf<T> {
-	ByteFee::get() * U::max_encoded_len().saturated_into()
+	ByteFee::get().saturating_mul(U::max_encoded_len().saturated_into())
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::mock::*;
+	use frame_support::pallet_prelude::Get;
+
+	struct Two;
+	impl Get<u128> for Two {
+		fn get() -> u128 {
+			2
+		}
+	}
+
+	#[test]
+	fn calculate_deposit_of_works() {
+		new_test_ext().execute_with(|| {
+			// 4 + 4 bytes.
+			#[derive(Copy, Clone, Debug, Encode, Eq, Decode, MaxEncodedLen, PartialEq, TypeInfo)]
+			struct Data {
+				pub a: u32,
+				pub b: u32,
+			}
+
+			// 8 * 2 = 16 units
+			assert_eq!(calculate_deposit_of::<Test, Two, Data>(), 16);
+		})
+	}
+
 }
