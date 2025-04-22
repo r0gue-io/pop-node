10a394586ca4d5a4af0ed524f15fea910b79a819
f-gate <felixgate13@gmail.com>
2025-04-22 16:12:24 +0100

    tests 2

--- pallets/api/src/messaging/tests.rs
+++ pallets/api/src/messaging/tests.rs
@@ -1432,12 +1432,33 @@ mod top_up_callback_weight {
 
 	#[test]
 	fn message_not_found() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let weight = Weight::from_parts(100_000, 100_000);
+			let message_id = [0u8; 32];
+			assert_noop!(
+				Messaging::top_up_callback_weight(signed(ALICE), message_id, weight),
+				Error::<Test>::MessageNotFound
+			);
+		})
 	}
 
 	#[test]
 	fn zero_weight_is_err() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let message_id = [0u8; 32];
+			let message = Message::IsmpResponse {
+				commitment: Default::default(),
+				deposit: 100,
+				response: Default::default(),
+			};
+			let weight = Weight::zero();
+
+			Messages::<Test>::insert(ALICE, message_id, message);
+			assert_noop!(
+				Messaging::top_up_callback_weight(signed(ALICE), message_id, weight),
+				Error::<Test>::ZeroWeight
+			);
+		})
 	}
 
 	#[test]
@@ -1539,6 +1560,7 @@ mod top_up_callback_weight {
 			));
 
 			if let Some(Message::Ismp { callback, .. }) = Messages::<Test>::get(ALICE, message_id) {
+				// Assert that weight has been accrued and deposit taken.
 				assert_eq!(callback.unwrap().weight, initial_weight + additional_weight);
 				let held = Balances::total_balance_on_hold(&ALICE);
 				assert_eq!(held, initial_fee + additional_fee);
@@ -1550,21 +1572,115 @@ mod top_up_callback_weight {
 
 	#[test]
 	fn ismp_pending_no_callback() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let message_id = [0u8; 32];
+			let message = Message::Ismp {
+				commitment: Default::default(),
+				deposit: 100,
+				callback: None,
+			};
+			let additional_weight = Weight::from_parts(100_000, 100_000);
+
+			Messages::<Test>::insert(ALICE, message_id, message);
+			assert_noop!(Messaging::top_up_callback_weight(
+				signed(ALICE),
+				message_id,
+				additional_weight
+			), Error::<Test>::NoCallbackFound);
+		})
 	}
 
 	#[test]
 	fn xcm_pending_no_callback() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let message_id = [0u8; 32];
+			let message = Message::XcmQuery {
+				query_id: 0,
+				deposit: 100,
+				callback: None,
+			};
+			let additional_weight = Weight::from_parts(100_000, 100_000);
+
+
+			Messages::<Test>::insert(ALICE, message_id, message);
+			assert_noop!(Messaging::top_up_callback_weight(
+				signed(ALICE),
+				message_id,
+				additional_weight
+			), Error::<Test>::NoCallbackFound);
+		})
 	}
 
 	#[test]
 	fn xcm_pending_works() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let message_id = [0u8; 32];
+			let initial_weight = Weight::from_parts(150_000, 150_000);
+			let initial_fee = <Test as Config>::WeightToFee::weight_to_fee(&initial_weight);
+			let callback = Callback { abi: Abi::Scale, selector: [0u8; 4], weight: initial_weight };
+			let message = Message::XcmQuery {
+				query_id: Default::default(),
+				deposit: 100,
+				callback: Some(callback),
+			};
+			let additional_weight = Weight::from_parts(100_000, 100_000);
+			let additional_fee = <Test as Config>::WeightToFee::weight_to_fee(&additional_weight);
+
+			// take initial hold
+			<Test as crate::messaging::Config>::Fungibles::hold(
+				&HoldReason::Messaging.into(),
+				&ALICE,
+				initial_fee,
+			)
+			.unwrap();
+
+			Messages::<Test>::insert(ALICE, message_id, message);
+
+			assert_ne!(additional_fee, 0);
+			assert_ne!(initial_fee, 0);
+			assert_ok!(Messaging::top_up_callback_weight(
+				signed(ALICE),
+				message_id,
+				additional_weight
+			));
+
+			if let Some(Message::XcmQuery { callback, .. }) = Messages::<Test>::get(ALICE, message_id) {
+				// Assert that weight has been accrued and deposit taken.
+				assert_eq!(callback.unwrap().weight, initial_weight + additional_weight);
+				let held = Balances::total_balance_on_hold(&ALICE);
+				assert_eq!(held, initial_fee + additional_fee);
+			} else {
+				panic!("Wrong message type or not found.");
+			}
+		})
 	}
 
 	#[test]
 	fn assert_event() {
-		new_test_ext().execute_with(|| {})
+		new_test_ext().execute_with(|| {
+			let message_id = [0u8; 32];
+			let initial_weight = Weight::from_parts(150_000, 150_000);
+			let callback = Callback { abi: Abi::Scale, selector: [0u8; 4], weight: initial_weight };
+			let message = Message::XcmQuery {
+				query_id: Default::default(),
+				deposit: 100,
+				callback: Some(callback),
+			};
+			let additional_weight = Weight::from_parts(100_000, 100_000);
+
+			Messages::<Test>::insert(ALICE, message_id, message);
+
+			assert_ok!(Messaging::top_up_callback_weight(
+				signed(ALICE),
+				message_id,
+				additional_weight
+			));
+
+			let events = events();
+			assert!(events.contains(&Event::<Test>::CallbackGasIncreased {
+				message_id,
+				total_weight: initial_weight + additional_weight,
+			}));
+		})
 	}
 }
