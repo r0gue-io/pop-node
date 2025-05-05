626888e8fa64cf06d0ab6b676249b4741a25e9ea
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-05-05 14:21:48 +0700

    chore: BlockNumberProvider for devnet nonfungibles

--- runtime/devnet/src/config/api/mod.rs
+++ runtime/devnet/src/config/api/mod.rs
@@ -88,6 +88,7 @@ impl fungibles::Config for Runtime {
 }
 
 impl nonfungibles::Config for Runtime {
+	type BlockNumberProvider = frame_system::Pallet<Runtime>;
 	type NftsInstance = TrustBackedNftsInstance;
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
