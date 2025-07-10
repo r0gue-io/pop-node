1b56a1c78c7f74c72afe2b8ca2fcfb9e851c6054
Frank Bell <frank@eri.rs>
2025-07-10 21:53:36 +0100

    fix(api): add missing imports after refactor

--- pop-api-vnext/src/fungibles/erc20.rs
+++ pop-api-vnext/src/fungibles/erc20.rs
@@ -1,6 +1,6 @@
 pub use v0::*;
 
-use super::{contract_ref, prefixed_address, Address, Pop, Sol, String, TokenId, U256};
+use super::{contract_ref, prefixed_address, Address, Pop, Sol, String, TokenId, Vec, U256};
 use crate::ensure;
 
 /// The first version of the Erc20 API.
