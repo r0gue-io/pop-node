9ed2280fb051da05a40d0e8bbc5e31a1d8c77b02
Frank Bell <frank@eri.rs>
2025-07-29 21:04:33 +0100

    refactor(devnet): add ismp weight provider

--- runtime/devnet/src/config/ismp.rs
+++ runtime/devnet/src/config/ismp.rs
@@ -21,7 +21,7 @@ impl pallet_ismp::Config for Runtime {
 	type Router = Router;
 	type RuntimeEvent = RuntimeEvent;
 	type TimestampProvider = Timestamp;
-	type WeightProvider = ();
+	type WeightProvider = WeightProvider;
 }
 
 impl ismp_parachain::Config for Runtime {
@@ -47,10 +47,24 @@ impl Get<StateMachine> for HostStateMachine {
 pub struct Router;
 impl IsmpRouter for Router {
 	fn module_for_id(&self, id: Vec<u8>) -> Result<Box<dyn IsmpModule>, anyhow::Error> {
-		use pallet_api_vnext::messaging::transports::ismp::{Module as Messaging, *};
-		if id == ID {
-			return Ok(Box::new(Messaging::<Runtime>::new()));
+		use pallet_api_vnext::messaging::transports::ismp as messaging;
+		match id {
+			id if id == messaging::ID => Ok(Box::new(messaging::Module::<Runtime>::new())),
+			_ => Err(Error::ModuleNotFound(id).into()),
 		}
-		Err(Error::ModuleNotFound(id).into())
+	}
+}
+
+pub struct WeightProvider;
+impl pallet_ismp::weights::WeightProvider for WeightProvider {
+	fn module_callback(
+		dest_module: pallet_ismp::ModuleId,
+	) -> Option<Box<dyn pallet_ismp::weights::IsmpModuleWeight>> {
+		use pallet_api_vnext::messaging::transports::ismp as messaging;
+		match dest_module.to_bytes() {
+			dest_module if dest_module == messaging::ID =>
+				Some(Box::new(messaging::Module::<Runtime>::new())),
+			_ => None,
+		}
 	}
 }
